67a4a83fdef096735c1cb3df2ceb5e8715165922
gichangee <93646339+gichangee@users.noreply.github.com>
2024-01-02 22:13:58 +0900

    [level 1] Title: 2016년, Time: 0.02 ms, Memory: 72.5 MB -BaekjoonHub

--- 프로그래머스/1/12901. 2016년/2016년.java
+++ 프로그래머스/1/12901. 2016년/2016년.java
@@ -1,11 +1,18 @@
 import java.time.LocalDate;
 
 class Solution {
-    private String[] week = {"MON", "TUE", "WED", "THU", "FRI", "SAT", "SUN"};
+  
+    public String solution(int a, int b) {
+        String answer = "";
+        String[] day = { "FRI", "SAT", "SUN", "MON", "TUE", "WED", "THU" };
+        int[] date = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+        int allDate = 0;
+        for (int i = 0; i < a - 1; i++) {
+            allDate += date[i];
+        }
+        allDate += (b - 1);
+        answer = day[allDate % 7];
 
-    public String solution(int month, int day) {
-        LocalDate date = LocalDate.of(2016, month, day);
-        int dayOfWeek = date.getDayOfWeek().ordinal();
-        return week[dayOfWeek];
+        return answer;
     }
 }
