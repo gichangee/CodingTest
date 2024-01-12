aae9ac210b73984c10f04d1b131ca9637140ea7f
gichangee <93646339+gichangee@users.noreply.github.com>
2024-01-12 17:21:18 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 512 ms, Memory: 23392 KB -BaekjoonHub

---
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+public class Main{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x= sc.nextInt();
+        
+        for(int i=0;i<x;i++){
+            for(int j=0;j<x-i-1;j++){
+                System.out.print(" ");
+            }
+            for(int j=0;j<i+1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=0;i<x;i++){
+            
+            for(int j=0;j<i+1;j++){
+                System.out.print(" ");
+            }
+             for(int j=0;j<x-i-1;j++){
+                System.out.print("*");
+            }
+            for(int j=0;j<x-i-2;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+   
+    }
+}
