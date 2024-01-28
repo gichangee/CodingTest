6bc177a7a21152f40ae6cbcd895a50968cee9ea6
gichangee <93646339+gichangee@users.noreply.github.com>
2024-01-28 21:46:23 +0900

    [Silver III] Title: N과 M (3), Time: 684 ms, Memory: 298780 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,53 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.util.*;
+
+public class Main {
+
+	static int N;
+	static int K;
+	static int[] arr;
+	static boolean[] v;
+	static BufferedWriter bw;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
+		
+		
+		N = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		
+		arr = new int[K];
+		v= new boolean[N];
+		dfs(0);
+		bw.flush();
+		bw.close();
+	}
+	private static void dfs(int index) throws IOException {
+		
+		if(index==K) {
+			for(int i=0;i<index;i++) {
+				bw.write(arr[i]+" ");
+			}
+			bw.write("\n");
+			return;
+		}
+		
+		
+		for(int i=1;i<=N;i++) {
+		
+			arr[index]=i;
+			dfs(index+1);
+
+		}
+			
+		
+	}
+
+}
