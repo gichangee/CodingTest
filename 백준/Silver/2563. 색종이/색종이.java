87f994bbc0ad29bf7fc844797c0c53774019ae8b
gichangee <93646339+gichangee@users.noreply.github.com>
2024-02-07 17:50:36 +0900

    [Silver V] Title: 색종이, Time: 124 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,35 +1,52 @@
-import java.io.*;
-public class Main{
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int t = Integer.parseInt(br.readLine());
-        
-        int[][] arr = new int[100][100];
-        
-        for(int i=0;i<t;i++){
-            String s = br.readLine();
-            
-            String[] sp = s.split(" ");
-            int a= Integer.parseInt(sp[0]);
-            int b =Integer.parseInt(sp[1]);
-            
-            for(int j=a;j<a+10;j++){
-                for(int z=b;z<b+10;z++){
-                    arr[j][z]++;
-                }
-            }
-        }
-        int sum=0;
-        for(int i=0;i<100;i++){
-            for(int j=0;j<100;j++){
-                if(arr[i][j]>0){
-                    sum++;
-                }
-            }
-        }
-        sb.append(sum);
-        System.out.println(sb.toString());
-        
-    }
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[][] arr =new int[100][100];
+		
+		for(int i=0;i<100;i++) {
+			for(int j=0;j<100;j++) {
+				arr[i][j]=0;
+			}
+		}
+		
+		int count= Integer.parseInt(br.readLine());
+		
+		
+		
+		for(int i=0;i<count;i++) {
+			StringTokenizer st= new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			
+			for(int k = a;k<a+10;k++) {
+				for(int z = b;z<b+10;z++) {
+					arr[k][z]++;
+				}
+			}
+			
+			
+		}
+		
+		int sum=0;
+		
+		for(int i=0;i<100;i++) {
+			for(int j=0;j<100;j++) {
+				if(arr[i][j]>0) {
+					sum++;
+				}
+			}
+		}
+		
+		System.out.println(sum);
+		
+	}
+
 }
