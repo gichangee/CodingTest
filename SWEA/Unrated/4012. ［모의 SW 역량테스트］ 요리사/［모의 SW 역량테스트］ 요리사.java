d8fe7c1a81d557aaf9d0e644c3663d70764adc71
gichangee <93646339+gichangee@users.noreply.github.com>
2024-02-09 01:27:41 +0900

    [Unrated] Title: [모의 SW 역량테스트] 요리사, Time: 527 ms, Memory: 107,968 KB -BaekjoonHub

--- SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
+++ SWEA/Unrated/4012. ［모의 SW 역량테스트］ 요리사/［모의 SW 역량테스트］ 요리사.java
@@ -6,6 +6,13 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.StringTokenizer;
 
+/**
+ * 
+ * @author 박기창
+ * 
+ * 조합 이용해서 풀기
+ *
+ */
 public class Solution {
 
 	static int size;
@@ -34,9 +41,17 @@ public class Solution {
 					arr[i][j] = Integer.parseInt(st.nextToken());
 				}
 			}
+			
+			//최솟값 설정
 			min = Integer.MAX_VALUE;
+			
+			//재료의 반을 저장할 배열
 			temp = new int[size / 2];
+			
+			//재료의 나머지 반을 저장할 배열
 			temp2 = new int[size / 2];
+			
+			//재료 나누기 시작
 			comb(0, 0);
 
 			System.out.println("#" + test_case + " " + min);
@@ -47,6 +62,7 @@ public class Solution {
 
 	private static void comb(int cnt, int start) {
 
+		//재료의 수가 반이 되었을 때
 		if (cnt == (size / 2)) {
 
 			boolean[] v = new boolean[size];
@@ -55,6 +71,7 @@ public class Solution {
 				v[temp[i]] = true;
 			}
 
+			//재료의 나머지 반 채우기
 			int index = 0;
 			for (int i = 0; i < size; i++) {
 				if (!v[i]) {
@@ -66,11 +83,13 @@ public class Solution {
 			
 			list = new ArrayList<int[]>();
 			cookarr = new int[2];
+			//해당 재료로 만들수 있는 요리 뽑기
 			cook(0, 0, temp);
 			cook(0, 0, temp2);
 
 			List<Integer> sumlist = new ArrayList<>();
 
+			//해당 재료 조합으로 만들 수 있는 모든 요리 저장하기
 			for (int i = 0; i < list.size(); i++) {
 				int sum = 0;
 				sum += arr[list.get(i)[0]][list.get(i)[1]];
@@ -78,22 +97,30 @@ public class Solution {
 				sumlist.add(sum);
 			}
 			
+			
+			//sumlist.size()/2 까지는 재료의 반을 이용한 요리 
 			int a= 0;
 			for(int i=0;i<sumlist.size()/2;i++) {
 				a+=sumlist.get(i);
 			}
+			
+			//그 나머지는 나머지 재료로 만든 요리 
 			int b=0;
 			for(int i=sumlist.size()/2;i<sumlist.size();i++) {
 				b+=sumlist.get(i);
 			}
+			
+			//재료와 나머지 재료로 만든 요리의 합 빼기 
 			int value = Math.abs(b-a);
 			
+			//최솟값찾기
 			if(value <= min) {
 				min=value;
 			}
 			
 
 			return;
+			
 		} else {
 			for (int i = start; i < size; i++) {
 				temp[cnt] = i;
@@ -102,6 +129,8 @@ public class Solution {
 		}
 	}
 
+	
+	//요리 조합 만들기
 	private static void cook(int cnt, int start, int[] temp3) {
 
 		if (cnt == 2) {
