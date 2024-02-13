09b8396300dc0f5134791c2e45b9be13cb3b5d64
gichangee <93646339+gichangee@users.noreply.github.com>
2024-02-13 14:29:30 +0900

    [Silver V] Title: 스네이크버드, Time: 268 ms, Memory: 20580 KB -BaekjoonHub

--- 백준/Silver/16435. 스네이크버드/스네이크버드.java
+++ 백준/Silver/16435. 스네이크버드/스네이크버드.java
@@ -1,6 +1,10 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+ * @author 박기창
+ * 정렬사용하기
+ */
 public class Main {
 
 	public static void main(String[] args) {
@@ -16,10 +20,11 @@ public class Main {
 		}
 		
 		
+		//과일을 올림차순으로 정렬
 		Arrays.sort(arr);
 		
 		
-		
+		//과일이 길이보다 작거나 같으면 먹고 먹었을 때 길이 +1 해주기
 		for(int i=0;i<arr.length;i++) {
 			if(initlen >= arr[i]) {
 				initlen++;
