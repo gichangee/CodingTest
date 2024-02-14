46bec237729c3199b221ccba68a867d5b955a14e
gichangee <93646339+gichangee@users.noreply.github.com>
2024-02-14 12:00:24 +0900

    [Silver IV] Title: 수 찾기, Time: 676 ms, Memory: 42472 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -1,55 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
+/**
+ * @author 박기창
+ * 자바 api binarySearch 사용
+ */
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
-		Scanner sc = new Scanner(System.in);
-		int cnt = sc.nextInt();
-		
-		int[] arr= new int[cnt];
-		for(int i=0;i<cnt;i++) {
-			arr[i]=sc.nextInt();
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int len = Integer.parseInt(br.readLine());
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		StringBuilder sb = new StringBuilder();
+		int[] arr = new int[len];
+		for (int i = 0; i < len; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
 		}
+
 		
+		//이진탐색을 하기 위해서는 반드시 배열은 정렬되어있어야함
 		Arrays.sort(arr);
-		int r = sc.nextInt();
+		int len2 = Integer.parseInt(br.readLine());
+
+		st = new StringTokenizer(br.readLine());
+
+		int[] arr2 = new int[len2];
+		for (int i = 0; i < len2; i++) {
+			arr2[i] = Integer.parseInt(st.nextToken());
+		}
 		
-		for(int i=0;i<r;i++) {
-			
-			boolean result = false;
-			
-			int k = sc.nextInt();
-			
-			int first = 0;
-			int end = arr.length-1;
-			
-			while(first<=end) {
-				
-				int mid = (first+end)/2;
-				
-				if(arr[mid] < k) {
-					first = mid+1;
-				}else if(arr[mid] > k) {
-					end = mid-1;
-				}else if(arr[mid] == k) {
-					
-					result=true;
-					break;
-				}
-				
-			}
-			
-			
-			
-			
-			if(!result) {
-				System.out.println("0");
+		
+		//만약 원본배열안에 찾고자 하는 값이 들어있으면 1을 붙이고 없으면 0을 붙인다.
+		for(int i=0;i<len2;i++) {
+			if(Arrays.binarySearch(arr, arr2[i])>=0) {
+				sb.append(1+"\n");
 			}else {
-				System.out.println("1");
+				sb.append(0+"\n");
 			}
 		}
+		
+		
+		System.out.println(sb);
 	}
 
 }
