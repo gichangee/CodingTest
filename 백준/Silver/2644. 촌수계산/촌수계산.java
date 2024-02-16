2df193c7dd04e168b979fcf1bdc2ca5e3021fbfa
gichangee <93646339+gichangee@users.noreply.github.com>
2024-02-16 17:53:32 +0900

    [Silver II] Title: 촌수계산, Time: 120 ms, Memory: 14168 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.java
+++ 백준/Silver/2644. 촌수계산/촌수계산.java
@@ -6,6 +6,10 @@ import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/**
+ * @author 박기창
+ * bfs 사용하기
+ */
 public class Main {
 
 	static int[][] arr;
@@ -26,6 +30,8 @@ public class Main {
 		arr = new int[N+1][N+1];
 		v = new boolean[N+1];
 		
+		
+		//인접행렬 사용하기
 		for (int i = 0; i < R; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
@@ -35,6 +41,7 @@ public class Main {
 		}
 		
 
+		//만약 트루값으로 나오면 카운값 출력 없으면 -1 출력
 		boolean a = loop(first, second);
 		if(a) {
 		}else {
@@ -43,9 +50,14 @@ public class Main {
 	}
 
 	private static boolean loop(int first, int second) {
+		
+		//큐 사용
 		Queue<int []> q = new ArrayDeque<>();
 		q.add(new int[] {first,0});
+		//방문 체크하기
 		v[first]=true;
+		
+		//큐가 빌때까지 돌기
 		while(!q.isEmpty()) {
 			int[] temp = q.poll();
 			if(temp[0] == second) {
