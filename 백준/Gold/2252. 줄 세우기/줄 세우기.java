4d1209ed042c83335f28e9c4b0f0eab04598ba6e
gichangee <93646339+gichangee@users.noreply.github.com>
2024-02-21 08:58:54 +0900

    [Gold III] Title: 줄 세우기, Time: 888 ms, Memory: 47772 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -6,6 +6,10 @@ import java.util.ArrayList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
+/**
+ * @author 박기창
+ * 위상정렬 사용하기
+ */
 public class Main {
 
 	public static void main(String[] args) throws IOException {
@@ -15,20 +19,27 @@ public class Main {
 		int N = Integer.parseInt(st.nextToken());
 		int R = Integer.parseInt(st.nextToken());
 		
+		//어레이리스트 만들고 그 안에 또 어레이 리스트 만들기
 		ArrayList<ArrayList<Integer>> list = new ArrayList<>();
 		for(int i=0;i<=N;i++) {
+			//어레이리스트 초기화하기
 			list.add(new ArrayList<>());
 		}
 		
 		
+		//진입차수를 기록할 배열 생성
 		int[] degree = new int[N+1];
 		for(int i=0;i<R;i++) {
 			st= new StringTokenizer(br.readLine());
 			int s = Integer.parseInt(st.nextToken());
 			int e = Integer.parseInt(st.nextToken());
+			//리스트에 값 넣기
 			list.get(s).add(e);
+			//진입차수가 있다면 증가
 			degree[e]++;
 		}
+		
+		//진입차수가 0을 큐에 일단 넣기
 		Queue<Integer> q = new ArrayDeque<>(); 
 		for(int i=1;i<degree.length;i++) {
 			if(degree[i]==0) {
@@ -37,6 +48,8 @@ public class Main {
 		}
 		
 		
+		//큐가 빌때까지 반복
+		//어레이리스트에 하나씩 꺼내면서 하나씩 해당 리스트에 있는거 하나씩 자르기
 		while(!q.isEmpty()) {
 			int x = q.poll();
 			System.out.print(x+" ");
