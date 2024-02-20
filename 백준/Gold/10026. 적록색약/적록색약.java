3de5c5bae46f405a47100accb9a37e6577ccf3bd
gichangee <93646339+gichangee@users.noreply.github.com>
2024-02-20 15:32:17 +0900

    [Gold V] Title: 적록색약, Time: 136 ms, Memory: 14728 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,96 +1,110 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+	public static int N, rgbcnt, rgcnt;
+	public static char[][] map;
+	public static boolean[][] rgbmap;
+	public static boolean[][] rgmap;
+	// 위, 오, 아, 왼
+	public static int dy[] = {-1,0,1,0};
+	public static int dx[] = {0,1,0,-1};
+	
+	public static class Node{
+		int y;
+		int x;
+		
+		Node(int y, int x) {
+			this.y = y;
+			this.x = x;
+		}
+	}
+	
+	public static void RGBCheck(int y, int x, char c) {
+		Queue<Node> que = new ArrayDeque<Node>();
+		que.offer(new Node(y,x));
+		rgbmap[y][x] = true;
+		
+		while(!que.isEmpty()) {
+			Node NewNode = que.poll();
+			
+			for(int i=0;i<4;i++) {
+				int fy = NewNode.y + dy[i];
+				int fx = NewNode.x + dx[i];
+				
+				if(fy<0 || fx<0 || fy>=N || fx>=N || map[fy][fx]!=c || rgbmap[fy][fx]) {
+					continue;
+				}
+				que.offer(new Node(fy,fx));
+				rgbmap[fy][fx] = true;			
+			}
+		}
+	}
+	
+	public static void RGCheck(int y, int x, char c) {
+		Queue<Node> que = new ArrayDeque<Node>();
+		que.offer(new Node(y,x));
+		rgmap[y][x] = true;
+		
+		while(!que.isEmpty()) {
+			Node NewNode = que.poll();
+			
+			for(int i=0;i<4;i++) {
+				int fy = NewNode.y + dy[i];
+				int fx = NewNode.x + dx[i];
+				
+				if(fy<0 || fx<0 || fy>=N || fx>=N || rgmap[fy][fx]) {
+					continue;
+				}
+				else if(c == 'B' && map[fy][fx]!='B') {
+					continue;
+				}
+				else if(c == 'R' || c == 'G') {
+					if(map[fy][fx]=='B')
+						continue;
+				}
+				
+				que.offer(new Node(fy,fx));
+				rgmap[fy][fx] = true;			
+			}
+		}
+	}
 
-	static int[] dy = { -1, 1, 0, 0 };
-	static int[] dx = { 0, 0, -1, 1 };
-	static char[][] arr;
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int size = Integer.parseInt(br.readLine());
-
-		arr = new char[size][size];
-		for (int i = 0; i < size; i++) {
-			arr[i] = br.readLine().toCharArray();
+		StringBuilder sb = new StringBuilder();
+		N = Integer.parseInt(br.readLine());
+		map = new char[N][N];
+		rgbmap = new boolean[N][N];
+		rgmap = new boolean[N][N];
+		
+		for(int i=0;i<N;i++) {
+			String str = br.readLine();
+			for(int j=0;j<N;j++) {
+				map[i][j] = str.charAt(j);
+			}
 		}
-		boolean[][] v = new boolean[size][size];
-
-		int count = 0;
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
-				if (!v[i][j]) {
-					nojang(v, i, j, arr[i][j]);
-					count++;
+		rgbcnt = 0;
+		rgcnt = 0;
+		for(int i=0;i<N;i++) {
+			for(int j=0;j<N;j++) {
+				if(!rgbmap[i][j]) {
+					RGBCheck(i,j,map[i][j]);
+					rgbcnt++;
+				}
+				
+				if(!rgmap[i][j]) {
+					RGCheck(i,j,map[i][j]);
+					rgcnt++;
 				}
 			}
 		}
-
-		System.out.print(count + " ");
-
-		v = new boolean[size][size];
-		count = 0;
-		for (int i = 0; i < arr.length; i++) {
-			for (int j = 0; j < arr.length; j++) {
-				if (!v[i][j]) {
-					yesjang(v, i, j, arr[i][j]);
-					count++;
-				}
-			}
-		}
-		System.out.print(count);
-	}
-
-	private static void nojang(boolean[][] v, int y, int x, char c) {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[] { y, x });
-		while (!q.isEmpty()) {
-			int[] temp = q.poll();
-			for (int i = 0; i < 4; i++) {
-				int goY = temp[0] + dy[i];
-				int goX = temp[1] + dx[i];
-
-				if (goX >= 0 && goY >= 0 && goX < v.length && goY < v.length) {
-					if (!v[goY][goX] && arr[goY][goX] == c) {
-						v[goY][goX] = true;
-						q.add(new int[] { goY, goX });
-					}
-				}
-			}
-
-		}
-
-	}
-
-	private static void yesjang(boolean[][] v, int y, int x, char c) {
-		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[] { y, x });
-		while (!q.isEmpty()) {
-			int[] temp = q.poll();
-			for (int i = 0; i < 4; i++) {
-				int goY = temp[0] + dy[i];
-				int goX = temp[1] + dx[i];
-
-				if (goX >= 0 && goY >= 0 && goX < v.length && goY < v.length) {
-					if(!v[goY][goX] && c != arr[goY][goX]) {
-						if(c=='R' && arr[goY][goX] =='G' || c=='G' && arr[goY][goX] =='R') {
-							v[goY][goX] = true;
-							q.add(new int[] { goY, goX });
-						}
-					}
-					else if (!v[goY][goX] && arr[goY][goX] == c) {
-						v[goY][goX] = true;
-						q.add(new int[] { goY, goX });
-					}
-				}
-			}
-
-		}
+		sb.append(rgbcnt).append("\n");
+		sb.append(rgcnt).append("\n");
+		System.out.print(sb);
 
 	}
 
