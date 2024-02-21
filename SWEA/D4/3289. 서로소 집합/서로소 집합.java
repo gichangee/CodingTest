66e1e2aa7c9f5879be2446ff3fd71348b582e2e7
gichangee <93646339+gichangee@users.noreply.github.com>
2024-02-21 13:16:32 +0900

    [D4] Title: 서로소 집합, Time: 434 ms, Memory: 101,956 KB -BaekjoonHub

--- SWEA/D4/3289. 서로소 집합/서로소 집합.java
+++ SWEA/D4/3289. 서로소 집합/서로소 집합.java
@@ -1,65 +1,101 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Solution {
-
-	static int[] arr;
-
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		int t = sc.nextInt();
-		for (int test_case = 1; test_case <= t; test_case++) {
-			int N = sc.nextInt();
-			int R = sc.nextInt();
-			StringBuilder sb = new StringBuilder();
-			arr = new int[N + 1];
-
-			for (int i = 1; i <=N; i++) {
-				arr[i] = i;
-			}
-
-			for (int i = 0; i < R; i++) {
-				int h = sc.nextInt();
-				int x = sc.nextInt();
-				int y = sc.nextInt();
-
-				if (h == 0) {
-					union(x, y);
-				} else if (h == 1) {
-					int a = find(x);
-					int b = find(y);
-					if (a == b) {
-						sb.append("1");
-					} else {
-						sb.append("0");
-					}
+	public static int N,M;
+	public static int[] treemap;
+	
+	public static void Make_Set() {
+		for(int i=1;i<=N;i++) {
+			treemap[i] = i;	// 처음 부모는 자기 자신 주입해주기
+		}
+	}
+	
+	public static int Find_Set(int x) {
+		// x의 부모노드 반환
+		int y = treemap[x];
+		while(y!=x) {
+			x = y;
+			y = treemap[x];
+		}
+		return y;
+	}
+	
+	public static int UnionFind_Set(int x,int z) {
+		// x의 부모노드 반환
+		int y = treemap[x];
+		treemap[x] = z;
+		while(y!=x) {
+			x = y;
+			y = treemap[x];
+			treemap[x] = z;
+		}
+		return y;
+	}
+	
+	public static void Union_Set(int x,int y) {
+		// y의 부모노드체크, x의 부모노드 체크 필요
+		
+		// 서로의 부모노드가 같다면 합칠 필요 없음.
+		int xparent = Find_Set(x);
+		int yparent = UnionFind_Set(y, xparent);
+		
+		
+		
+		if(xparent!=yparent) {
+			treemap[yparent] = xparent;
+		}
+		
+//		int xparent = Find_Set(x);
+//		int yparent = Find_Set(y);
+//		if(xparent!=yparent) {
+//			treemap[yparent] = xparent;
+//			if(treemap[y]!=xparent) {
+//				UnionFind_Set(y,xparent);
+//			}
+//		}
+	}
+	
+	public static void main(String[] args)throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int rtc = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		for(int tc = 1;tc<=rtc;tc++) {
+			String str = br.readLine();
+			StringTokenizer stz = new StringTokenizer(str);
+			sb.append("#").append(tc).append(" ");
+			N = Integer.parseInt(stz.nextToken());
+			M = Integer.parseInt(stz.nextToken());
+			treemap = new int[N+1];
+			
+			Make_Set();
+			
+			for(int i=0;i<M;i++) {
+				str = br.readLine();
+				stz = new StringTokenizer(str);
+				int num = Integer.parseInt(stz.nextToken());
+				//합집합 -> Union
+				if(num == 0) {
+					int x = Integer.parseInt(stz.nextToken());
+					int y = Integer.parseInt(stz.nextToken());
+					Union_Set(x,y);
 				}
+				// 교집합 -> FindSet
+				else if(num == 1) {
+					int x = Integer.parseInt(stz.nextToken());
+					int y = Integer.parseInt(stz.nextToken());
+					x = Find_Set(x);
+					y = Find_Set(y);
+					if(x == y) 
+						sb.append("1");
+					else
+						sb.append("0");
+				}
+				
 			}
-
-			System.out.println("#" + test_case + " " + sb.toString());
-
+			sb.append("\n");
 		}
+		System.out.print(sb);
 	}
-
-	private static int find(int x) {
-
-		if (arr[x] == x) {
-			return x;
-		} else {
-			return arr[x] = find(arr[x]);
-		}
-
-	}
-
-	private static void union(int x, int y) {
-
-		int a = find(x);
-		int b = find(y);
-		if (a != b) {
-			arr[b] = a;
-		}
-
-	}
-
 }
