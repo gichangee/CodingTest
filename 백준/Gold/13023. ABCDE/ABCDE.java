15c0c593c3dc66d11344a0b5580035b88265c8c8
gichangee <93646339+gichangee@users.noreply.github.com>
2024-02-21 21:36:00 +0900

    [Gold V] Title: ABCDE, Time: 300 ms, Memory: 21416 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -2,16 +2,18 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 /**
- * @author 박기창
- * 인접리스트 사용하기
+ * @author 박기창 인접리스트 사용하기
  */
 public class Main {
 
 	static int N;
 	static boolean a;
+	static boolean[] v;
+	static List<Integer>[] list;
 
 	public static void main(String[] args) throws IOException {
 
@@ -21,61 +23,54 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		int R = Integer.parseInt(st.nextToken());
 
-		ArrayList<ArrayList<Integer>> list = new ArrayList<>();
+		list = new ArrayList[N];
 
-		//리스트 초기화 하기
+		// 리스트 초기화 하기
 		for (int i = 0; i < N; i++) {
-			list.add(new ArrayList<>());
+			list[i] = new ArrayList<>();
 		}
 
-		
-		//인접리스트에 값 넣기
+		// 인접리스트에 값 넣기
 		for (int i = 0; i < R; i++) {
 			st = new StringTokenizer(br.readLine());
 			int x = Integer.parseInt(st.nextToken());
 			int y = Integer.parseInt(st.nextToken());
-			list.get(x).add(y);
-			list.get(y).add(x);
+			list[x].add(y);
+			list[y].add(x);
 		}
-		
-		
-		boolean[] v = new boolean[N];
+
 		a = false;
-		for(int i=0;i<N;i++) {
+		for (int i = 0; i < N; i++) {
 			v = new boolean[N];
-            v[i]=true;
-			if(dfs(list, v, i, 0)>=4){
-                a=true;
-                break;
-            }
-			
+			v[i] = true;
+			dfs(i, 0);
+			if (a) {
+				System.out.println("1");
+				break;
+			}
+
 		}
-		if (a) {
-			System.out.println("1");
-		} else {
+
+		if (!a) {
 			System.out.println("0");
 
 		}
-		
-
-		
 
 	}
 
-	private static int dfs(ArrayList<ArrayList<Integer>> list, boolean[] v, int cnt, int depth) {
+	private static void dfs(int cnt, int depth) {
 
-		if (depth >= 4) {
-			return depth;
+		if (depth == 4) {
+			a = true;
+			return;
 		}
-	    int d=0;
-		for (int i = 0; i < list.get(cnt).size(); i++) {
-			if (!v[list.get(cnt).get(i)]) {
-				v[list.get(cnt).get(i)] = true;
-				d=Math.max(d,dfs(list, v, list.get(cnt).get(i), depth + 1));
-				v[list.get(cnt).get(i)] = false;
+		for (int i = 0; i < list[cnt].size(); i++) {
+			if (!v[list[cnt].get(i)]) {
+				v[list[cnt].get(i)] = true;
+				dfs(list[cnt].get(i), depth + 1);
+				v[list[cnt].get(i)] = false;
 			}
 		}
-        return d;
 	}
 
 }
