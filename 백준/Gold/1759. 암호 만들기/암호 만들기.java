48799c33846b84d3fff31a03708b0063e461e6f9
gichangee <93646339+gichangee@users.noreply.github.com>
2024-02-21 22:39:09 +0900

    [Gold V] Title: 암호 만들기, Time: 176 ms, Memory: 15100 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,78 +1,63 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static int moum = 0;
-	public static int L,C;
-	public static char[] cmap;
-	public static StringBuilder sb = new StringBuilder();
+
 	
-	public static void dfs(int depth, int i,int secreat, String strsecreat) {
-		
-		if(depth == L) {
-			//모음 1개이상 포함되었는지 
-			if((secreat&moum) == 0) {
-				return;
-			}
-			
-			//자음 2개이상 포함되어있는지 체크
-			int cnt = 0;
-			for(int j=0;j<26;j++) {
-				if( j == ('a' - 'a') || j == ('e' - 'a') 
-						|| j ==  ('i' - 'a') || j == ('o' - 'a') 
-						|| j == ('u' - 'a')) {
-					continue;
-				}
-				if((secreat&(1<<j))!=0) {
-					cnt++;
-				}
-				if(cnt>=2)
-					break;
-			}
-			
-			if(cnt>=2)
-				sb.append(strsecreat).append("\n");
-			
-			
-			return;
+	static int len,R;
+	static char[] arr,temp;
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		len = Integer.parseInt(st.nextToken());
+		R = Integer.parseInt(st.nextToken());
+		arr = new char[R];
+		String s = br.readLine();
+		String[] split = s.split(" ");
+		Arrays.sort(split);
+		for (int i = 0; i < R; i++) {
+			arr[i] = split[i].charAt(0);
 		}
 		
-		for(;i<C;i++) {
-			String stringtotal  = strsecreat + cmap[i];
-			int total = (secreat | (1<<(cmap[i] - 'a')));
-			dfs(depth+1,i+1,total,stringtotal);
-		}
+		temp = new char[len];
+		combine(0,0);
+
 	}
 
-	public static void main(String[] args) throws Exception{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		StringTokenizer stz = new StringTokenizer(str);
-		
-		L = Integer.parseInt(stz.nextToken());
-		C = Integer.parseInt(stz.nextToken());
-		cmap = new char[C];
-		str = br.readLine();
-		str = str.replaceAll("\\s", "");
-		
-		for(int i=0;i<C;i++) {
-			cmap[i] = str.charAt(i);	
-		}
-		//정렬
-		Arrays.sort(cmap);
-		
-		for(int i=0;i<26;i++) {
-			if( i == ('a' - 'a') || i == ('e' - 'a') 
-					|| i ==  ('i' - 'a') || i == ('o' - 'a') 
-					|| i == ('u' - 'a')) {
-				moum = moum|(1<<i);
+	private static void combine(int cnt, int start) {
+		if(cnt == len) {
+			
+			boolean a= false;
+			int count=0;
+			for(int i=0;i<cnt;i++) {
+				if(temp[i]=='a' || temp[i]=='e' ||temp[i]=='i' ||temp[i]=='o' ||temp[i]=='u' ){
+					a=true;
+				}
+				else {
+					count++;
+				}
+			}
+			
+			if(a && count>=2) {
+				for(int i=0;i<cnt;i++) {
+					System.out.print(temp[i]);
+				}
+				System.out.println();
+			}
+			
+			
+		}else {
+			for(int i=start;i<arr.length;i++) {
+				temp[cnt]=arr[i];
+				combine(cnt+1,i+1);
 			}
 		}
-		dfs(0,0,0,"");
-		System.out.print(sb);
-
+		
 	}
 
 }
