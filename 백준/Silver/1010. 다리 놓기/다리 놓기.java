314c01c5a1eb087a61ce52fdbf3ae78e3c579c52
gichangee <93646339+gichangee@users.noreply.github.com>
2024-02-28 12:05:38 +0900

    [Silver V] Title: 다리 놓기, Time: 92 ms, Memory: 12392 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,60 +1,35 @@
-
-
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
 
-	static int west;
-	static int east;
-	static int count;
-	
-	public static void main(String[] args) throws IOException {
-
+	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
 		StringBuilder sb = new StringBuilder();
-
-		int t = Integer.parseInt(st.nextToken());
-
-		// 0은 사용 안하고 1 부터 30까지 사용
-		int[][] arr = new int[31][31];
+		int rtc = Integer.parseInt(br.readLine());
 		
-		//대각선 값 채우기
-		for(int i=1;i<=30;i++) {
-			for(int j=1;j<=30;j++) {
-				arr[i][j]=1;
+		for(int tc=0;tc<rtc;tc++) {
+			StringTokenizer stz = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(stz.nextToken());
+			int M = Integer.parseInt(stz.nextToken());
+			int[][] dp = new int[N+2][M+2];
+			
+			//맨아래 1로 채우기
+			for(int i=M+1;i>0;i--) {
+				dp[N+1][i] = 1;
 			}
-		}
-		
-		//왼쪽 한 줄 채우기
-		for(int i=1;i<=30;i++) {
-			arr[i][1]=i;
-		}
-		
-		// nCr = n-1Cr + n-1Cr-1
-		for(int i=3;i<=30;i++) {
-			for(int j=2;j<i;j++) {
-				arr[i][j]= arr[i-1][j]+arr[i-1][j-1];
+			
+			for(int i=N,k=0;i>=1;i--,k++) {
+				//첫 시작값은 무조건 1임
+				//dp[i][M-k] = 1;
+				for(int j=M-k;j>0;j--) {
+					dp[i][j] = dp[i][j+1] + dp[i+1][j+1];
+				}
 			}
+			sb.append(dp[1][1]).append("\n");
 		}
-		
-		
-		
-		
-		for (int i = 0; i < t; i++) {
-			st = new StringTokenizer(br.readLine());
-			west = Integer.parseInt(st.nextToken());
-			east = Integer.parseInt(st.nextToken());
-		
-			sb.append(arr[east][west]+"\n");
-		}
-		System.out.println(sb.toString());
-	
-
+		System.out.print(sb);
 	}
-
 
 }
