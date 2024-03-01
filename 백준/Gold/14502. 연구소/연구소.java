822f3af0f5d15a731eaf0f1d4e2aaab15b826efa
gichangee <93646339+gichangee@users.noreply.github.com>
2024-03-02 00:52:44 +0900

    [Gold IV] Title: 연구소, Time: 876 ms, Memory: 301476 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -1,131 +1,116 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	public static class Node{
-		int y;
-		int x;
-		Node(int y, int x){
-			this.y = y;
-			this.x = x;
-		}
-	}
+	static int[] dy= {-1,1,0,0};
+	static int[] dx= {0,0,-1,1};
 	
-	public static int N,M;
-	public static int[][] map;
-	public static boolean[][] bmap;
-	public static int answer;
-	public static Node[] nodes;
-	//우, 하, 좌, 상
-	public static int[] dy = {0,1,0,-1};
-	public static int[] dx = {1,0,-1,0};
+	static int y,x;
 	
-	public static void Running() {
-		int cnt = 0;
-		//BFS 해줄 큐 생성
-		Queue<Node> que = new ArrayDeque<Node>();
-		
-		//복사배열 선언
-		int[][] copymap = new int[N][M];
-		boolean[][] bcopymap = new boolean[N][M];
-		
-		//벽세워주기
-		for(int i=0;i<3;i++) {
-			copymap[nodes[i].y][nodes[i].x] = 1;
-		}
-		
-		// 맵복사
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<M;j++) {
-				if(copymap[i][j]!=0)
-					continue;
+	static List<int[]> virus;
+	
+	static int[][] arr,copy;
+	
+	static int max;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		y = Integer.parseInt(st.nextToken());
+		x = Integer.parseInt(st.nextToken());
+		arr = new int[y][x];
+		copy = new int[y][x];
+
+		List<int[]> wall = new ArrayList<>();
+		virus = new ArrayList<>();
+
+		for (int i = 0; i < y; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 0; j < x; j++) {
+				arr[i][j] = Integer.parseInt(st.nextToken());
+				copy[i][j] = arr[i][j];
+				if (arr[i][j] == 0) {
+					wall.add(new int[] { i, j });
+				}
 				
-				copymap[i][j] = map[i][j];
-				
-				if(copymap[i][j] == 2) {
-					que.offer(new Node(i, j));
-					bcopymap[i][j] = true;
+				if(arr[i][j]==2) {
+					virus.add(new int[] {i,j});
 				}
 			}
 		}
 		
-		//동작
-		while(!que.isEmpty()) {
-			Node node = que.poll();
+		max = Integer.MIN_VALUE;
+		
+		int[] temp = new int[wall.size()];
+		boolean[] v = new boolean[wall.size()];
+		loop(wall,temp,arr,v,0);
+		System.out.println(max);
+	}
+
+	private static void loop(List<int[]> wall, int[] temp, int[][] arr, boolean[] v, int cnt) {
+		if(cnt==3) {
+			for(int i=0;i<temp.length;i++) {
+				if(temp[i]==1) {
+					arr[wall.get(i)[0]][wall.get(i)[1]]=1;
+				}
+			}
 			
+			bfs();
+			int count=0;
+			for(int i=0;i<y;i++) {
+				for(int j=0;j<x;j++) {
+					if(arr[i][j]==0) {
+						count++;
+					}
+				}
+			}
+			
+			if(max < count) {
+				max = count;
+			}
+			
+			for(int i=0;i<y;i++) {
+				for(int j=0;j<x;j++) {
+					arr[i][j] = copy[i][j];
+				}
+			}
+			
+			return;
+		}else {
+			for(int i=0;i<temp.length;i++) {
+				if(!v[i]) {
+					v[i]=true;
+					temp[i]=1;
+					loop(wall, temp, arr, v, cnt+1);
+					temp[i]=0;
+					v[i]=false;
+				}
+				
+			}
+		}
+		
+	}
+
+	private static void bfs() {
+
+		Queue<int []> q = new ArrayDeque<int[]>();
+		for(int i=0;i<virus.size();i++) {
+			q.add(new int[] {virus.get(i)[0],virus.get(i)[1]});
+		}
+		
+		while(!q.isEmpty()) {
+			int[] temp = q.poll();
 			for(int i=0;i<4;i++) {
-				int fy = node.y + dy[i];
-				int fx = node.x + dx[i];
-				
-				if(fy<0 || fx<0 || fy>=N || fx>=M || 
-						bcopymap[fy][fx] || copymap[fy][fx]!=0 )
-					continue;
-				
-				copymap[fy][fx] = 2;
-				bcopymap[fy][fx] = true;
-				que.offer(new Node(fy, fx));
-				
-			}
-			
-		}
-		
-		
-		//카운트 체크
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<M;j++) {
-				if(copymap[i][j] == 0)
-					cnt++;
+				int goY = temp[0]+ dy[i];
+				int goX = temp[1]+ dx[i];
+				if(goY>=0 && goY<y && goX>=0 && goX<x && arr[goY][goX]==0) {
+					arr[goY][goX]=2;
+					q.add(new int[] {goY,goX});
+				}
 			}
 		}
 		
-		if(cnt>answer)
-			answer = cnt;
-	}
-	
-	public static void dfs(int depth,int i,int j) {
-		
-		if(depth >=3) {
-			Running();
-			return; 
-		}
-		
-		for(;i<N;i++) {
-			for(;j<M;j++) {
-				if(map[i][j]!=0 || bmap[i][j])
-					continue;
-				bmap[i][j] = true;
-				nodes[depth] = new Node(i, j);
-				dfs(depth+1,i,j+1);
-				bmap[i][j] = false;
-			}
-			j=0;
-		}
-		
-	}
-	
-	
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer stz = new StringTokenizer(br.readLine()," ");
-		N = Integer.parseInt(stz.nextToken());
-		M = Integer.parseInt(stz.nextToken());
-		map = new int[N][M];
-		bmap = new boolean[N][M];
-		nodes = new Node[3];
-		
-		for(int i=0;i<N;i++) {
-			stz = new StringTokenizer(br.readLine()," ");
-			for(int j=0;j<M;j++) {
-				map[i][j] = Integer.parseInt(stz.nextToken());
-			}
-		}
-		answer = 0;
-		dfs(0,0,0);
-		System.out.println(answer);
 	}
 
 }
