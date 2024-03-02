e1652cde98f88caaeddab8f116766334d84c288f
gichangee <93646339+gichangee@users.noreply.github.com>
2024-03-03 02:54:54 +0900

    [D4] Title: 최소 스패닝 트리, Time: 1,908 ms, Memory: 113,900 KB -BaekjoonHub

--- SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
+++ SWEA/D4/3124. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,14 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
+/**
+ * @author 박기창
+ * 크루스칼 알고리즘 사용하기
+ * 간선리스트 사용하기
+ * union find 사용하기
+ */
+import java.util.*;
+import java.io.*;
 public class Solution {
 
 	static int V, E;
 	static int[] parents;
+	
+	//간선정보 담을 클래스 만들기
 	static class Edge implements Comparable<Edge> {
 
 		int from;
@@ -21,6 +24,8 @@ public class Solution {
 			this.weigth = weigth;
 		}
 
+		
+		//클래스 정렬을 가중치 올림차순으로 만들기
 		@Override
 		public int compareTo(Edge o) {
 
@@ -37,10 +42,16 @@ public class Solution {
 		for (int test_case = 1; test_case <= t; test_case++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 
+			
+			//노드 개수 간선 개수 입력받기
 			V = Integer.parseInt(st.nextToken());
 			E = Integer.parseInt(st.nextToken());
 
+			
+			//간선 개수만큼 배열 만들기
 			Edge[] edges = new Edge[E];
+			
+			//정보 입력받고 간선 배열에 넣기
 			for (int i = 0; i < E; i++) {
 				st = new StringTokenizer(br.readLine());
 				int from = Integer.parseInt(st.nextToken());
@@ -50,22 +61,36 @@ public class Solution {
 			}
 			
 			
-			
+			//가중치 기준으로 올림차순 정렬하기
 			Arrays.sort(edges);
 			
+			
+			//노드 자기 자신으로 초기화 하기
 			parents = new int[V+1];
 			for(int i=1;i<=V;i++) {
 				parents[i]=i;
 			}
 			
+			//가중치
 			long weight =0;
+			
+			//간선 개수
 			int cnt = 0;
+			
+			//엣지클래스 배열안에 있는 간선 정보 모두 탐색하기
 			for(Edge e : edges) {
+				
+				//만약 합집합을 안해도 되는 상태면 넘어가기 이미 같은 대표값을 가르키고 있을 때
 				if(!union(e.from,e.to)) {
 					continue;
 				}else {
+					// 합치는거 성공하면 가중치 더해주기
 					weight += e.weigth;
+					
+					//간선개수 늘리기
 					cnt++;
+					
+					//간선개수가 v-1만큼 되면 종료하기
 					if(cnt == V-1) {
 						break;
 					}
@@ -78,6 +103,8 @@ public class Solution {
 
 	}
 
+	
+	//union
 	private static boolean union(int from, int to) {
 		int x = find(from);
 		int y = find(to);
