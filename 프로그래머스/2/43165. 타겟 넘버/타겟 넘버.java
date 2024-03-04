eee3964d432693ad3b11c818bcd039d07893154e
gichangee <93646339+gichangee@users.noreply.github.com>
2024-03-04 23:43:59 +0900

    [level 2] Title: 타겟 넘버, Time: 0.44 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,31 +1,24 @@
 class Solution {
     
-    int[] num;
-    int target;
-    int answer;
-    
+    static int answer;
     public int solution(int[] numbers, int target) {
-    
-        answer=0;
-        this.target=target;
-        this.num=numbers;
+        answer = 0;
         
-        dfs(0,0);
+        
+        dfs(0,0,numbers,target);
         
         return answer;
     }
     
-    public void dfs(int index,int sum){
-        if(index == num.length){
+    static void dfs(int cnt, int sum,int[] numbers, int target){
+        if(cnt == numbers.length){
             if(sum==target){
                 answer++;
-               
-            } return;
+            }
+            return;
+        }else{
+            dfs(cnt+1,sum+numbers[cnt],numbers,target);
+            dfs(cnt+1,sum-numbers[cnt],numbers,target);
         }
-        dfs(index+1,sum+num[index]);
-        dfs(index+1,sum-num[index]);
     }
-    
-    
- 
 }
