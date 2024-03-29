5e9e2ce40dd8482a9bd30fcd10a1ef64500f42d4
gichangee <93646339+gichangee@users.noreply.github.com>
2024-03-30 08:40:10 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 1,222 ms, Memory: 119,908 KB -BaekjoonHub

---
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/README.md
@@ -0,0 +1,15 @@
+# [Unrated] [모의 SW 역량테스트] 벽돌 깨기 - 5656 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWXRQm6qfL0DFAUo) 
+
+### 성능 요약
+
+메모리: 119,908 KB, 시간: 1,222 ms, 코드길이: 3,458 Bytes
+
+### 제출 일자
+
+2024-03-30 08:40
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -0,0 +1,161 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+	static int N, W, H;
+	static int min;
+	static int dy[] = { -1, 1, 0, 0 };
+	static int dx[] = { 0, 0, -1, 1 };
+	static int[][] copy;
+	
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= t; test_case++) {
+
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			N = Integer.parseInt(st.nextToken());
+			W = Integer.parseInt(st.nextToken());
+			H = Integer.parseInt(st.nextToken());
+
+			min = Integer.MAX_VALUE;
+
+			int[][] arr = new int[H][W];
+			copy = new int[H][W];
+
+			for (int i = 0; i < H; i++) {
+				st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < W; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+					copy[i][j]=arr[i][j];
+				}
+			}
+
+			// 중복순열
+			int[] temp = new int[N];
+
+			permutation(arr, temp, 0);
+
+			System.out.println("#" + test_case + " " + min);
+		}
+	}
+
+	private static void permutation(int[][] arr, int[] temp, int depth) {
+
+		if (temp.length == depth) {
+
+			boolean[][] v = new boolean[arr.length][arr[0].length];
+			
+			for (int i = 0; i < H; i++) {
+				for (int j = 0; j < W; j++) {
+					arr[i][j]=copy[i][j];
+				}
+			}
+			
+
+			for (int i = 0; i < temp.length; i++) {
+
+				Queue<int[]> blockList = new ArrayDeque<>();
+				Queue<int[]> q = new ArrayDeque<>();
+				// 세로줄에서 위에서부터 0이 아닌 값 찾기
+				for (int j = 0; j < H; j++) {
+					if (arr[j][temp[i]] > 0) {
+						blockList.add(new int[] {j, temp[i],arr[j][temp[i]]});
+						q.add(new int[] {j, temp[i]});
+						v[j][temp[i]]=true;
+						break;
+					}
+				}
+				
+				if(blockList.size()==0) {
+					continue;
+				}
+				
+				if (!blockList.isEmpty()) {
+					boom(blockList, arr,v,q);
+				}
+				
+			
+
+				while(!q.isEmpty()) {
+					int[] temp1 = q.poll();
+					arr[temp1[0]][temp1[1]] = 0;
+				}
+
+
+				
+			
+				
+				for (int k = 0; k < W; k++) {
+					//아래에서부터 블록을 확인해서 저장할 큐
+					Queue<Integer> sort = new ArrayDeque<>();
+					//아래에서 부터 블록을 큐에 저장하고 해당 블록을 지운다.
+					for (int j = H - 1; j >= 0; j--) {
+						if (arr[j][k] != 0) {
+							sort.offer(arr[j][k]);
+							arr[j][k] = 0;
+						}
+					}
+					//아래에서부터 블록을 쌓는다.
+					for (int j = H - 1; !sort.isEmpty(); j--) {
+						v[j][k]=false;
+						arr[j][k] = sort.poll();
+					}
+
+				}
+				
+			
+			}
+
+			
+
+			int count = 0;
+			for (int i = 0; i < H; i++) {
+				for (int j = 0; j < W; j++) {
+					if (arr[i][j] > 0) {
+						count++;
+					}
+				}
+			}
+
+			if (min > count) {
+				min = count;
+			}
+			
+			
+
+		} else {
+			for (int i = 0; i < W; i++) {
+				temp[depth] = i;
+				permutation(arr, temp, depth + 1);
+			}
+		}
+	}
+
+	private static void boom(Queue<int[]> blockList, int[][] arr, boolean[][] v, Queue<int[]> q) {
+		// 터질 블록이 있으면
+				while (!blockList.isEmpty()) {
+					int[] temp = blockList.poll();
+					// 터질 블록 레벨만큼 4방의 블록을 큐에 넣음
+					for (int i = 1; i < temp[2]; i++) {
+						for (int j = 0; j < 4; j++) {
+							int ny = temp[0] + dy[j] * i;
+							int nx = temp[1] + dx[j] * i;
+							if (ny >= 0 && nx >= 0 && nx < W && ny < H && arr[ny][nx] != 0) {
+								if(!v[ny][nx]) {
+									blockList.add(new int[] {ny,nx,arr[ny][nx]});
+									q.add(new int[] {ny,nx});
+									v[ny][nx] = true;
+								}
+
+							}
+						}
+					}
+				}
+			
+
+	}
+
+}
