1b13489f93eef8ae3550e46b12ed28680a717f22
gichangee <93646339+gichangee@users.noreply.github.com>
2024-04-02 11:52:57 +0900

    [D4] Title: 수영대회 결승전 ( 완전 탐색 + 구현 ), Time: 114 ms, Memory: 23,892 KB -BaekjoonHub

--- SWEA/D4/4193. 수영대회 결승전 （ 완전 탐색 ＋ 구현 ）/수영대회 결승전 （ 완전 탐색 ＋ 구현 ）.java
+++ SWEA/D4/4193. 수영대회 결승전 （ 완전 탐색 ＋ 구현 ）/수영대회 결승전 （ 완전 탐색 ＋ 구현 ）.java
@@ -1,63 +1,118 @@
 import java.util.*;
-class Solution {
-    static int answer;
-    static int[][] map, visited;
-    static int arrx, arry;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
+import java.io.*;
 
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T = sc.nextInt();
-        for (int test_case = 1; test_case <= T; test_case++) {
-            int x = sc.nextInt();
-            map = new int[x][x];
-            visited = new int[x][x];
-            answer = -1;
-            for (int i = 0; i < map.length; i++) {
-                for (int j = 0; j < map[0].length; j++) {
-                    map[i][j] = sc.nextInt();
-                }
-            }
-            int startx = sc.nextInt();
-            int starty = sc.nextInt();
-            arrx = sc.nextInt();
-            arry = sc.nextInt();
-            bfs(startx, starty, 0);
-            System.out.println("#" + test_case + " " + answer);
-        }
-    }
-    public static void bfs(int startX, int startY, int time) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{startX, startY, time});
-        visited[startX][startY] = 1; 
-        while (!q.isEmpty()) {
-            int[] current = q.poll();
-            int x = current[0];
-            int y = current[1];
-            int currentTime = current[2];
-            if (x == arrx && y == arry) {
-                answer = currentTime;
-                return;
-            }
-            for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (nx >= 0 && ny >= 0 && nx < map.length && ny < map[0].length && map[nx][ny] != 1 && visited[nx][ny] == 0) {
-                   if (map[nx][ny] == 2) {
-                        if (currentTime % 3 == 2) {
-                            q.add(new int[]{nx, ny, currentTime + 1});
-                            visited[nx][ny] = 1; 
-                        } else{
-                        	 q.add(new int[]{x, y, currentTime + 1});
-                        }
-                    } else {
-                        q.add(new int[]{nx, ny, currentTime + 1});
-                        visited[nx][ny] = 1;  
-                    }
-                }
-            }
-        }
-    }
+public class Solution {
+
+	static int[][] arr;
+	static int starty, startx, endy, endx, count, N;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			N = Integer.parseInt(br.readLine());
+			arr = new int[N][N];
+
+			for (int i = 0; i < N; i++) {
+				StringTokenizer st = new StringTokenizer(br.readLine());
+				for (int j = 0; j < N; j++) {
+					arr[i][j] = Integer.parseInt(st.nextToken());
+				}
+			}
+
+			StringTokenizer st = new StringTokenizer(br.readLine());
+
+			starty = Integer.parseInt(st.nextToken());
+			startx = Integer.parseInt(st.nextToken());
+
+			st = new StringTokenizer(br.readLine());
+
+			endy = Integer.parseInt(st.nextToken());
+			endx = Integer.parseInt(st.nextToken());
+
+			
+			count=-1;
+			
+			dfs();
+
+			System.out.println("#" + test_case + " "+count);
+
+		}
+	}
+
+	private static void dfs() {
+
+		Queue<int[]> q = new ArrayDeque<>();
+		q.add(new int[] {starty,startx,0});
+		
+		boolean[][] v = new boolean[N][N];
+		
+		v[starty][startx] = true;
+		int[] dy = {-1,1,0,0};
+		int[] dx = {0,0,-1,1};
+		
+		Label: while(!q.isEmpty()) {
+			int[] temp = q.poll();
+			
+			
+			
+			
+			for(int i=0;i<4;i++) {
+				int goY = dy[i] + temp[0];
+				int goX = dx[i] + temp[1];
+				int h = temp[2];
+				
+				if(goY == endy && goX==endx) {
+					count = h+1;
+					break Label;
+				}
+				
+				if(goY>=0 && goY < N && goX >=0 && goX <N) {
+					if(!v[goY][goX]) {
+						//그냥 바다에 있을 때
+						if(arr[temp[0]][temp[1]]==0) {
+							//그냥 바다에 있을 때 그냥 바다로 갈 때
+							if(arr[goY][goX]==0) {
+								v[goY][goX]=true;
+								q.add(new int[] {goY,goX,h+1});
+							
+							//그냥 바다에 있을 때 소용돌이로 갈 때	
+							}else {
+								if((h+1)%3 !=0) {
+									q.add(new int[] {temp[0],temp[1],h+1});
+								}else {
+									v[goY][goX]=true;
+									q.add(new int[] {goY,goX,h+1});
+								}
+							}
+						//소용돌이 위에 있을 때
+						}else if(arr[temp[0]][temp[1]]==2){
+							
+							
+							if(arr[goY][goX]==0) {
+								v[goY][goX]=true;
+								q.add(new int[] {goY,goX,h+1});
+
+							}else {
+								if((h+1)%3 !=0) {
+									q.add(new int[] {temp[0],temp[1],h+1});
+								}else {
+									v[goY][goX]=true;
+									q.add(new int[] {goY,goX,h+1});
+								}
+							}
+							
+
+							
+						}
+					
+						
+					}
+				}
+			}
+			
+		}
+	}
+
 }
