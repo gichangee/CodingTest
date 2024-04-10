a9a1a88d2d9ba42942bfc36e0f3043bfa190ab9a
gichangee <93646339+gichangee@users.noreply.github.com>
2024-04-10 23:20:41 +0900

    [Silver II] Title: DFS와 BFS, Time: 344 ms, Memory: 24352 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,97 +1,78 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static StringBuilder sb;
-	public static int N,M,V;
-	public static boolean[][] bmap;
-	
-	public static void DFS() {
-		boolean[] bcheckmap = new boolean[N+1];
-		Stack<Integer> sta = new Stack<>();
-		sta.push(V);
-		sb.append(V).append(" ");
-		bcheckmap[V] = true;
-		
-		while(!sta.isEmpty()) {
-			int CheckNode = sta.peek();
-			
-			boolean isin = false;
-			for(int i = 1;i<=N;i++) {
-				if(i == CheckNode)
-					continue;
-				
-				if(bmap[CheckNode][i] && !bcheckmap[i]) {
-					bcheckmap[i] = true;
-					sta.push(i);
-					sb.append(i).append(" ");
-					isin = true;
-					break;
-				}
-			}
-			
-			if(!isin) {
-				sta.pop();
-			}
+
+	static int arr[][];
+	static boolean[] v;
+	static boolean[] dfsv;
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int N = Integer.parseInt(st.nextToken());
+		int R = Integer.parseInt(st.nextToken());
+		int start = Integer.parseInt(st.nextToken());
+
+		arr = new int[N + 1][N + 1];
+
+		v = new boolean[N + 1];
+
+		for (int i = 0; i < R; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			arr[x][y] = 1;
+			arr[y][x] = 1;
 		}
-	}
-	
-	public static void BFS() {
-		boolean[] bcheckmap = new boolean[N+1];
+
+		dfsv = new boolean[N+1];
 		
-		Queue<Integer> que = new ArrayDeque<Integer>();
-		bcheckmap[V] = true;
-		que.offer(V);
-		sb.append(V).append(" ");
-		
-		while(!que.isEmpty()) {
-			int topd = que.poll();
-			
-			for(int i=1;i<=N;i++) {
-				if(i == topd)
-					continue;
-				
-				if(bmap[topd][i] && !bcheckmap[i]) {
-					bcheckmap[i] = true;
-					que.offer(i);
-					sb.append(i).append(" ");
-				}
-			}
-			
-		}
+		dfs(start);
+		System.out.println();
+		bfs(start);
 	}
 
-	public static void main(String[] args) throws Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		StringTokenizer stz = new StringTokenizer(str);
-		sb  = new StringBuilder();
-		
-		N = Integer.parseInt(stz.nextToken());
-		M = Integer.parseInt(stz.nextToken());
-		V = Integer.parseInt(stz.nextToken());
-		
-		bmap = new boolean[N+1][10005];
-		
-		for(int i=0;i<M;i++) {
-			str = br.readLine();
-			stz = new StringTokenizer(str);
-			int v1 = Integer.parseInt(stz.nextToken());
-			int v2 = Integer.parseInt(stz.nextToken());
-			bmap[v1][v2] = true;
-			bmap[v2][v1] = true;
+	
+	
+	private static void dfs(int start) {
+		dfsv[start]=true;
+		System.out.print(start+" ");
+		for(int i=1;i<arr.length;i++) {
+			if(arr[start][i] !=0 && !dfsv[i]) {
+				
+				dfsv[i]=true;
+				dfs(i);
+			}
 		}
 		
-		DFS();
-		sb.append("\n");
-		BFS();
-		
-		System.out.print(sb);
+	}
+
+
+
+	private static void bfs(int start) {
+		Queue<Integer> q = new ArrayDeque<>();
+		q.add(start);
+		v[start] =true;
+		while (!q.isEmpty()) {
+			int x = q.poll();
+			
+			System.out.print(x+" ");
+			for(int i=1;i<arr.length;i++) {
+				if(arr[x][i] !=0  && !v[i]) {
+					
+					q.add(i);
+					
+					v[i]=true;
+				}
+			}
+		}
 	}
 
 }
