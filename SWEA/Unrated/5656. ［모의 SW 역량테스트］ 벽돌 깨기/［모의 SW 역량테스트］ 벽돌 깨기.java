3735981e30ffb361f18adb73de5f9aeaf218b64f
gichangee <93646339+gichangee@users.noreply.github.com>
2024-04-11 14:48:54 +0900

    [Unrated] Title: [모의 SW 역량테스트] 벽돌 깨기, Time: 1,381 ms, Memory: 117,364 KB -BaekjoonHub

--- SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
+++ SWEA/Unrated/5656. ［모의 SW 역량테스트］ 벽돌 깨기/［모의 SW 역량테스트］ 벽돌 깨기.java
@@ -3,145 +3,126 @@ import java.util.*;
 
 public class Solution {
 
-	static int N, W, H;
-	static int min;
-	static int dy[] = { -1, 1, 0, 0 };
-	static int dx[] = { 0, 0, -1, 1 };
-	static int[][] copy;
+	static int N, H, W;
+
+	static int[][] arr,copy;
 	
+	static int min;
+
+	static int[] dy = { -1, 1, 0, 0 };
+	static int[] dx = { 0, 0, -1, 1 };
+
 	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine());
-		for (int test_case = 1; test_case <= t; test_case++) {
-
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			N = Integer.parseInt(st.nextToken());
 			W = Integer.parseInt(st.nextToken());
 			H = Integer.parseInt(st.nextToken());
 
-			min = Integer.MAX_VALUE;
-
-			int[][] arr = new int[H][W];
+			arr = new int[H][W];
 			copy = new int[H][W];
-
 			for (int i = 0; i < H; i++) {
 				st = new StringTokenizer(br.readLine());
+
 				for (int j = 0; j < W; j++) {
 					arr[i][j] = Integer.parseInt(st.nextToken());
 					copy[i][j]=arr[i][j];
 				}
 			}
+			
+			min = Integer.MAX_VALUE;
 
-			// 중복순열
+			// 구슬 떨어뜨릴 순서 0 이면 0번째 세로줄 1이면 1번째 세로줄
 			int[] temp = new int[N];
+			loop(temp, 0);
+			System.out.println("#"+tc+" "+min);
 
-			permutation(arr, temp, 0);
-
-			System.out.println("#" + test_case + " " + min);
 		}
 	}
 
-	private static void permutation(int[][] arr, int[] temp, int depth) {
+	private static void loop(int[] temp, int depth) {
 
-		if (temp.length == depth) {
+		if (depth == temp.length) {
 
-			boolean[][] v = new boolean[arr.length][arr[0].length];
 			
-			for (int i = 0; i < H; i++) {
-				for (int j = 0; j < W; j++) {
+			for(int i=0;i<H;i++) {
+				for(int j=0;j<W;j++) {
 					arr[i][j]=copy[i][j];
 				}
 			}
 			
+			Queue<int[]> brisk = new ArrayDeque<>();
 
+			// 구슬 떨어뜨리기
 			for (int i = 0; i < temp.length; i++) {
 
-				Queue<int[]> blockList = new ArrayDeque<>();
-				Queue<int[]> q = new ArrayDeque<>();
+				// 위에서부터 해당 세로줄에 0이 아닌값 찾기
 				for (int j = 0; j < H; j++) {
-					if (arr[j][temp[i]] > 0) {
-						blockList.add(new int[] {j, temp[i],arr[j][temp[i]]});
-						q.add(new int[] {j, temp[i]});
-						v[j][temp[i]]=true;
+					if (arr[j][temp[i]] != 0) {
+						// 만약 찾았을 때
+						brisk.add(new int[] { j, temp[i], arr[j][temp[i]] });
 						break;
 					}
 				}
-				
-				if(blockList.size()==0) {
-					continue;
-				}
-				
-				if (!blockList.isEmpty()) {
-					boom(blockList, arr,v,q);
-				}
-				
-				while(!q.isEmpty()) {
-					int[] temp1 = q.poll();
-					arr[temp1[0]][temp1[1]] = 0;
-				}
-				
-				for (int k = 0; k < W; k++) {
-					Queue<Integer> sort = new ArrayDeque<>();
-					for (int j = H - 1; j >= 0; j--) {
-						if (arr[j][k] != 0) {
-							sort.offer(arr[j][k]);
-							arr[j][k] = 0;
-						}
-					}
-					for (int j = H - 1; !sort.isEmpty(); j--) {
-						v[j][k]=false;
-						arr[j][k] = sort.poll();
-					}
 
-				}
-
-
-			}
-
-			int count = 0;
-			for (int i = 0; i < H; i++) {
-				for (int j = 0; j < W; j++) {
-					if (arr[i][j] > 0) {
-						count++;
-					}
-				}
-			}
-
-			if (min > count) {
-				min = count;
-			}
-
-		} else {
-			for (int i = 0; i < W; i++) {
-				temp[depth] = i;
-				permutation(arr, temp, depth + 1);
-			}
-		}
-	}
-
-	private static void boom(Queue<int[]> blockList, int[][] arr, boolean[][] v, Queue<int[]> q) {
-		// 터질 블록이 있으면
-				while (!blockList.isEmpty()) {
-					int[] temp = blockList.poll();
-					// 터질 블록 레벨만큼 4방의 블록을 큐에 넣음
-					for (int i = 1; i < temp[2]; i++) {
-						for (int j = 0; j < 4; j++) {
-							int ny = temp[0] + dy[j] * i;
-							int nx = temp[1] + dx[j] * i;
-							if (ny >= 0 && nx >= 0 && nx < W && ny < H && arr[ny][nx] != 0) {
-								if(!v[ny][nx]) {
-									blockList.add(new int[] {ny,nx,arr[ny][nx]});
-									q.add(new int[] {ny,nx,arr[ny][nx]});
-									v[ny][nx] = true;
+				// 벽돌 부셔버리기
+				while (!brisk.isEmpty()) {
+					int[] position = brisk.poll();
+					int power = position[2];
+					arr[position[0]][position[1]] = 0;
+					for (int j = 0; j < 4; j++) {
+						for (int z = 1; z < power; z++) {
+							int goY = position[0] + dy[j] * z;
+							int goX = position[1] + dx[j] * z;
+							if (goY >= 0 && goX >= 0 && goY < H && goX < W) {
+								if (arr[goY][goX] != 0) {
+									brisk.add(new int[] { goY, goX, arr[goY][goX] });
+									arr[goY][goX] = 0;
 								}
-
 							}
 						}
 					}
 				}
-			
 
+				// 벽돌 내리기
+				for (int j = 0; j < W; j++) {
+					Queue<Integer> q = new ArrayDeque<>();
+					for (int z = H - 1; z >= 0; z--) {
+						if (arr[z][j] != 0) {
+							q.add(arr[z][j]);
+							arr[z][j] = 0;
+						}
+
+					}
+					int count = H - 1;
+					while (!q.isEmpty()) {
+						arr[count--][j] = q.poll();
+					}
+
+				}
+			}
+			
+			int c = 0;
+			for(int i=0;i<H;i++) {
+				for(int j=0;j<W;j++) {
+					if(arr[i][j]>0) {
+						c++;
+					}
+				}
+			}
+			
+			if(min > c) {
+				min = c;
+			}
+		} else {
+			for (int i = 0; i < W; i++) {
+				temp[depth] = i;
+				loop(temp, depth + 1);
+			}
+		}
 	}
 
 }
