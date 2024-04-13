b3af95ad869ec0a91234c0e0a8ac5e541180fef0
gichangee <93646339+gichangee@users.noreply.github.com>
2024-04-13 13:16:51 +0900

    [Silver II] Title: 유기농 배추, Time: 164 ms, Memory: 16064 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,75 +1,66 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
 
-	public static class Pair{
-		int y;
-		int x;
-		
-		Pair(int y, int x){
-			this.y = y;
-			this.x = x;
-		}
-	}
-	
-	public static void main(String[] args)throws Exception {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int rtc = Integer.parseInt(br.readLine());
-		//우, 하, 좌, 상
-		int[] dy = {0,1,0,-1};
-		int[] dx = {1,0,-1,0};
-		
-		
-		for(int tc = 1;tc<=rtc;tc++) {
-			String str = br.readLine();
-			StringTokenizer stz = new StringTokenizer(str);
-			int M = Integer.parseInt(stz.nextToken());
-			int N = Integer.parseInt(stz.nextToken());
-			int K = Integer.parseInt(stz.nextToken());
-			Queue<Pair> que = new ArrayDeque<Pair>();
-			boolean[][] map = new boolean[N+1][M+1];
+		int T = Integer.parseInt(br.readLine());
+		for (int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int M = Integer.parseInt(st.nextToken());
+			int N = Integer.parseInt(st.nextToken());
+			int K = Integer.parseInt(st.nextToken());
+			int[][] arr = new int[N][M];
+			for (int i = 0; i < K; i++) {
+				st = new StringTokenizer(br.readLine());
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				arr[y][x] = 1;
 			
-			for(int k=0;k<K;k++) {
-				str = br.readLine();
-				stz = new StringTokenizer(str);
-				int a = Integer.parseInt(stz.nextToken());
-				int b = Integer.parseInt(stz.nextToken());
-				map[b][a] = true;
 			}
-			int answer = 0;
-			boolean[][] bmap = new boolean[N+1][M+1];
+
+			boolean[][] v = new boolean[N][M];
+			int count=0;
+			
 			for(int i=0;i<N;i++) {
 				for(int j=0;j<M;j++) {
-					if(map[i][j] && !bmap[i][j]) {
-						bmap[i][j] = true;
-						answer++;
-						// BFS
-						que.offer(new Pair(i, j));
-						
-						while(!que.isEmpty()) {
-							Pair nowp = que.poll();
-							for(int k=0;k<4;k++) {
-								int fy = nowp.y + dy[k];
-								int fx = nowp.x + dx[k];
-								
-								if(fy<0 || fx<0 || fy>=N || fx>=M || bmap[fy][fx] || !map[fy][fx])
-									continue;
-								
-								bmap[fy][fx] = true;
-								que.offer(new Pair(fy, fx));
-							}
-						}
+					if(!v[i][j] && arr[i][j] ==1) {
+						v[i][j]=true;
+						bfs(arr,v,i,j);
+						count++;
 					}
 				}
 			}
-			sb.append(answer).append("\n");
+
+			System.out.println(count);
 		}
-		System.out.print(sb);
+	}
+
+	private static void bfs(int[][] arr, boolean[][] v, int y, int x) {
+
+		Queue<int[]> q = new ArrayDeque<>();
+		q.add(new int[] {y,x});
+		
+		int[] dy = {-1,1,0,0};
+		int[] dx = {0,0,-1,1};
+		while(!q.isEmpty()) {
+			int[] temp = q.poll();
+			
+			for(int i=0;i<4;i++) {
+				int goY = dy[i]+temp[0];
+				int goX = dx[i]+temp[1];
+				if(goY >=0 && goX >=0 && goY < arr.length && goX <arr[0].length) {
+					if(!v[goY][goX] && arr[goY][goX]==1) {
+						v[goY][goX]=true;
+						q.add(new int[] {goY,goX});
+					}
+				}
+			}
+			
+			
+		}
 	}
 
 }
