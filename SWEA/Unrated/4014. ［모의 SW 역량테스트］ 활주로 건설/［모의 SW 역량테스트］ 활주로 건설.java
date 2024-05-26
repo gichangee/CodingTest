d0fee8fc2002a3721a4258db7e5e936f43357d9d
gichangee <93646339+gichangee@users.noreply.github.com>
2024-05-27 00:04:45 +0900

    [Unrated] Title: [모의 SW 역량테스트] 활주로 건설, Time: 205 ms, Memory: 30,532 KB -BaekjoonHub

--- SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
+++ SWEA/Unrated/4014. ［모의 SW 역량테스트］ 활주로 건설/［모의 SW 역량테스트］ 활주로 건설.java
@@ -3,14 +3,17 @@ import java.util.*;
 
 public class Solution {
 
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws NumberFormatException, IOException {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
 		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
+
+		for (int TC = 1; TC <= T; TC++) {
 			StringTokenizer st = new StringTokenizer(br.readLine());
 			int N = Integer.parseInt(st.nextToken());
 			int X = Integer.parseInt(st.nextToken());
+
 			int[][] arr = new int[N][N];
 
 			for (int i = 0; i < N; i++) {
@@ -22,165 +25,148 @@ public class Solution {
 
 			int count = 0;
 
-			
-			
-			
-			// 가로줄 검사
+			// 가로
+			// 값이 진행하다가 달라졌을 때만 진행
+			boolean[][] v = new boolean[N][N];
 			for (int i = 0; i < N; i++) {
-
-				boolean[][] v = new boolean[N][N];
-				// 왼쪽 -> 오른쪽
-				// 가로줄 첫번째 값
-				int temp = arr[i][0];
-				Lable: for (int j = 1; j < N; j++) {
-					// 만약 비교값과 진행값이 같으면 그대로 넘어가기
-					if (temp == arr[i][j]) {
-						// 비교값 업데이트
-						temp = arr[i][j];
-						if (j == N - 1) {
-							count++;
-						}
+				int first = arr[i][0];
+				boolean a = true;
+				for (int j = 1; j < N; j++) {
+					// 활주로 건설이 안되는 경우에는 그냥 다음 행으로 넘어가기
+					if (!a) {
+						break;
+					}
+					int k = X;
+					int length = 0;
+					// 같다면 넘어가기
+					if (first == arr[i][j]) {
 						continue;
-						// 만약 비교값보다 진행값이 더 크다면 현재 비교값 위치부터
-						// 뒤로 경사로가 설치되는지 확인해보기
-						// 설치 안되면 그대로 해당줄은 탈락!
-					} else if (arr[i][j] - temp == 1) {
-						int len = 0;
-						
-						for (int z = j - 1; z >= 0; z--) {
-							
-							if (temp == arr[i][z] && !v[i][z]) {
-								v[i][z]=true;
-								len++;
-							}
-							// 경사로가 만들어지면 반복문 빠져나가기 temp값 업데이트
-							if (len == X) {
-								temp = arr[i][j];
+					}
+					// 앞에 값보다 크다면
+					// 앞에가 경사로가 설치되는지 검사
+					else if (first - arr[i][j] == -1) {
+						int c = 0;
+						for (int z = j - 1; z >= 0 && k > 0; z--, k--) {
+							if (first != arr[i][z]) {
+								a = false;
 								break;
-							}
-							// 만약 경사로가 다 만들어지기전에 끝나면 해당 줄은 탈락
-							if (temp != arr[i][z]) {
-								break Lable;
-							}
-						}
-						if (len < X) {
-							break Lable;
-						}
-
-						// 만약 비교값보다 진행값이 더 작다면 현재 진행값 위치부터
-						// 앞으로 경사로가 설치되는지 확인하기
-						// 설치 안되면 그대로 해당줄은 탈락!
-					} else if (temp - arr[i][j] == 1) {
-						int len = 1;
-						v[i][j]=true;
-						for (int z = j + 1; z < N; z++) {
-							if (arr[i][j] == arr[i][z] && !v[i][z]) {
-								v[i][z]=true;
-								len++;
-							}
-
-							if (len == X) {
-								temp = arr[i][j];
+							} else if (v[i][z]) {
+								a = false;
 								break;
-							}
-							if (arr[i][j] != arr[i][z]) {
-								break Lable;
+							} else {
+								v[i][z] = true;
+								c++;
 							}
 						}
-						if (len < X) {
-							break Lable;
+						first = arr[i][j];
+						if (c != X) {
+							a = false;
+							break;
+						}
+					}
+					// 앞에 값보다 작다면
+					// 뒤에게 경사로가 설치 되는지 검사
+					else if (first - arr[i][j] == 1) {
+						first = arr[i][j];
+						int c = 0;
+						// 그냥 x만큼 반복 돌리면 되는거 아님
+						// x만큼 못 돌면 경사로 못만든거
+						for (int z = 0; z < X && j < N; z++, j++) {
+							if (first != arr[i][j]) {
+								a = false;
+								break;
+							} else {
+								v[i][j] = true;
+								c++;
+							}
+						}
+						j--;
+						if (c != X) {
+							a = false;
 						}
 					} else {
-						break Lable;
+						a = false;
+						break;
 					}
-					
-					if (j == N - 1) {
-						count++;
-					}
-
+				}
+				if (a) {
+					count++;
 				}
 
 			}
 
-			// 세로줄 검사
+			// 세로
+			v = new boolean[N][N];
 			for (int i = 0; i < N; i++) {
-				boolean[][] v = new boolean[N][N];
-				// 위 -> 아래
-				// 세로줄 첫번째 값
-				int temp = arr[0][i];
-				Lable: for (int j = 1; j < N; j++) {
-					
-					// 만약 비교값과 진행값이 같으면 그대로 넘어가기
-					if (temp == arr[j][i]) {
-						// 비교값 업데이트
-						temp = arr[j][i];
-						if (j == N - 1) {
-							count++;
-						}
+				int first = arr[0][i];
+				boolean a = true;
+				for (int j = 1; j < N; j++) {
+					if (!a) {
+						break;
+					}
+					int k = X;
+					int length = 0;
+					// 같다면 넘어가기
+					if (first == arr[j][i]) {
 						continue;
-						// 만약 비교값보다 진행값이 더 크다면 현재 비교값 위치부터
-						// 뒤로 경사로가 설치되는지 확인해보기
-						// 설치 안되면 그대로 해당줄은 탈락!
-					} else if (arr[j][i] - temp == 1) {
-						int len = 0;
-						for (int z = j - 1; z >= 0; z--) {
-
-							if (temp == arr[z][i]  && !v[z][i]) {
+					}
+					
+					
+					// 앞에 값보다 크다면
+					// 앞에가 경사로가 설치되는지 검사
+					else if (first - arr[j][i] == -1) {
+						int c = 0;
+						for (int z = j - 1; z >= 0 && k > 0; z--, k--) {
+							if (first != arr[z][i]) {
+								a = false;
+								break;
+							} else if (v[z][i]) {
+								a = false;
+								break;
+							} else {
 								v[z][i] = true;
-								len++;
+								c++;
 							}
-							// 경사로가 만들어지면 반복문 빠져나가기 temp값 업데이트
-							if (len == X) {
-								temp = arr[j][i];
+						}
+						first = arr[j][i];
+						if (c != X) {
+							a = false;
+							break;
+						}
+					}
+					// 앞에 값보다 작다면
+					// 뒤에게 경사로가 설치 되는지 검사
+					else if (first - arr[j][i] == 1) {
+						first = arr[j][i];
+						int c = 0;
+						// 그냥 x만큼 반복 돌리면 되는거 아님
+						// x만큼 못 돌면 경사로 못만든거
+						for (int z = 0; z < X && j < N; z++, j++) {
+							if (first != arr[j][i]) {
+								a = false;
 								break;
-							}
-							// 만약 경사로가 다 만들어지기전에 끝나면 해당 줄은 탈락
-							if (temp != arr[z][i]) {
-								break Lable;
-							}
-						}
-						if (len < X) {
-							break Lable;
-						}
-
-						// 만약 비교값보다 진행값이 더 작다면 현재 진행값 위치부터
-						// 앞으로 경사로가 설치되는지 확인하기
-						// 설치 안되면 그대로 해당줄은 탈락!
-					} else if (temp - arr[j][i] == 1) {
-						int len = 1;
-						v[j][i] = true;
-						for (int z = j + 1; z < N; z++) {
-							if (arr[j][i] == arr[z][i] && !v[z][i]) {
-								v[z][i]=true;
-								len++;
-							}
-
-							if (len == X) {
-								temp = arr[j][i];
-								break;
-							}
-							if (arr[j][i] != arr[z][i]) {
-								break Lable;
+							} else {
+								v[j][i] = true;
+								c++;
 							}
 						}
-						if (len < X) {
-							break Lable;
+						j--;
+						if (c != X) {
+							a = false;
 						}
 					} else {
-						break Lable;
+						a = false;
+						break;
 					}
-
-					if (j == N - 1) {
-						count++;
-					}
-
 				}
-
+				
+				if (a) {
+					count++;
+				}
 			}
 
-			System.out.println("#" + tc + " " + count);
+			System.out.println("#" + TC + " " + count);
 		}
-
 	}
 
 }
