6ad2ab0c06b8d92236cd7b12e18eb3a0119c1e2d
gichangee <93646339+gichangee@users.noreply.github.com>
2024-05-28 17:57:29 +0900

    [Unrated] Title: [모의 SW 역량테스트] 점심 식사시간, Time: 150 ms, Memory: 37,552 KB -BaekjoonHub

--- SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
+++ SWEA/Unrated/2383. ［모의 SW 역량테스트］ 점심 식사시간/［모의 SW 역량테스트］ 점심 식사시간.java
@@ -1,127 +1,195 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Solution {
 
-	static int[] temp;
-	static int N,humanscnt,stairscnt;
-	static int[][] arr, stairs, humans;
 	static int min;
 
-	public static void main(String[] args) throws IOException {
+	static class Person {
+		int x;
+		int y;
+		int move = 0;
+
+		public Person(int y, int x) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+
+	static class Stair {
+		int x;
+		int y;
+		int len;
+
+		public Stair(int y, int x, int len) {
+			this.x = x;
+			this.y = y;
+			this.len = len;
+		}
+	}
+
+	static List<Person> personlist;
+	static List<Stair> stairlist;
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int T = Integer.parseInt(br.readLine());
 		for (int tc = 1; tc <= T; tc++) {
-			N = Integer.parseInt(br.readLine());
-			arr = new int[N][N];
 
-			
-			stairs = new int[2][3];
-			humans = new int[10][2];
-			
-			humanscnt = 0;
-			stairscnt = 0;
-			
+			min = Integer.MAX_VALUE;
+			int N = Integer.parseInt(br.readLine());
+			int[][] arr = new int[N][N];
+			personlist = new ArrayList<>();
+			stairlist = new ArrayList<>();
 			for (int i = 0; i < N; i++) {
 				StringTokenizer st = new StringTokenizer(br.readLine());
 				for (int j = 0; j < N; j++) {
 					arr[i][j] = Integer.parseInt(st.nextToken());
-					//사람인 경우
-					if(arr[i][j]==1) {
-						humans[humanscnt][0]= i;
-						humans[humanscnt][1]= j;
-						humanscnt++;
-						//계단인 경우
-					}else if(arr[i][j]>=2) {
-						stairs[stairscnt][0] = i;
-						stairs[stairscnt][1] = j;
-						stairs[stairscnt][2] = arr[i][j];
-						stairscnt++;
+					if (arr[i][j] == 1) {
+						personlist.add(new Person(i, j));
+					} else if (arr[i][j] > 1) {
+						stairlist.add(new Stair(i, j, arr[i][j]));
 					}
 				}
 			}
-			
-			min = Integer.MAX_VALUE;
-			temp = new int[humanscnt];
-			
-			loop(0);
-			
-			System.out.println("#"+tc+" "+min);
-			
-		}
 
+			int[] temp = new int[personlist.size()];
+
+			per(temp, 0);
+
+			System.out.println("#" + tc + " " + min);
+
+		}
 	}
 
-	private static void loop(int cnt) {
+	private static void per(int[] temp, int depth) {
 
-		if(cnt == humanscnt) {
-			
-			List<Integer> stair0 = new ArrayList<>();
-			List<Integer> stair1 = new ArrayList<>();
-			
-			
-			for(int i=0;i<temp.length;i++) {
-				if(temp[i]==0) {
-					int d = Math.abs(humans[i][0]-stairs[0][0]) + Math.abs(humans[i][1]-stairs[0][1]);
-					stair0.add(d);
-				}else {
-					int d = Math.abs(humans[i][0]-stairs[1][0]) + Math.abs(humans[i][1]-stairs[1][1]);
-					stair1.add(d);
+		if (temp.length == depth) {
+
+			// 일단 첫번쨰 계단과 두번째 계단으로 들어가는 그룹 2개로 나누기
+			List<int[]> first = new ArrayList<>();
+			List<int[]> second = new ArrayList<>();
+
+			for (int i = 0; i < temp.length; i++) {
+				if (temp[i] == 0) {
+					int d = Math.abs(personlist.get(i).y - stairlist.get(0).y)
+							+ Math.abs(personlist.get(i).x - stairlist.get(0).x);
+					first.add(new int[] { personlist.get(i).y, personlist.get(i).x, d });
+				} else {
+					int d = Math.abs(personlist.get(i).y - stairlist.get(1).y)
+							+ Math.abs(personlist.get(i).x - stairlist.get(1).x);
+					second.add(new int[] { personlist.get(i).y, personlist.get(i).x, d });
 				}
 			}
-			
-			
-			Collections.sort(stair0);
-			Collections.sort(stair1);
-			
-			
-			int minute = Math.max(arrivetime(stair0,0), arrivetime(stair1,1));
-			
-			if(min>minute) {
-				min = minute;
-			}
 
-			return;
-		}else {
-			for(int i=0;i<2;i++) {
-				temp[cnt]=i;
-				loop(cnt+1);
-			}
-		}
-	}
+			// 일단 거리순으로 정렬하기
+			Collections.sort(first, new Comparator<int[]>() {
+				@Override
+				public int compare(int[] o1, int[] o2) {
 
-	private static int arrivetime(List<Integer> stair, int stairnumber) {
-		
-		
-		if(stair.size()==0) {
-			return 0;
-		}
-		int[] dp = new int[stair.size()];
-		
-		if(stair.size()<=3) {
-			for(int i=0;i<stair.size();i++) {
-				dp[i] = stairs[stairnumber][2]+1+stair.get(i);
-			}
-			
-		}else {
-			for(int i=0;i<3;i++) {
-				dp[i] = stairs[stairnumber][2]+1+stair.get(i);
-			}
-			
-			for(int i=3;i<stair.size();i++) {
-				if(dp[i-3] > stair.get(i)) {
-					dp[i] = dp[i-3]+stairs[stairnumber][2];
-				}else {
-					dp[i] = stairs[stairnumber][2]+1+stair.get(i);
+					return o1[2] - o2[2];
+				}
+			});
+			Collections.sort(second, new Comparator<int[]>() {
+				@Override
+				public int compare(int[] o1, int[] o2) {
+
+					return o1[2] - o2[2];
+				}
+			});
+
+//			for (int i = 0; i < first.size(); i++) {
+//				System.out.print(first.get(i)[2] + " ");
+//			}
+//			System.out.println();
+
+			// first 계단 내려오는데 걸린 시간이랑
+			// second 계단에 내려오는데 걸린 시간중
+			// 더 긴걸 뽑으면 됨
+
+			int firsttime = 0;
+			int secondtime = 0;
+
+			// 계단의 상태를 저장할 무언가가 있어야한다.-> person 객체를 담는다
+			List<Person> currentstair = new ArrayList<>();
+
+			while (first.size() != 0) {
+				// 계단길이 == move 확인 -> 계단 이동 -> 계단에 도착한 사람 리스트에 넣어주기
+				//
+				for (int i = 0; i < currentstair.size(); i++) {
+					if (currentstair.get(i).move == stairlist.get(0).len) {
+						currentstair.remove(i);
+						first.remove(i);
+						i--;
+					}
+				}
+
+				// 계단에 내려오게하기
+				for (int i = 0; i < currentstair.size(); i++) {
+					if (i >= 3)
+						break;
+					else {
+						currentstair.get(i).move++;
+					}
+				}
+				for (int i = 0; i < first.size(); i++) {
+					if (first.get(i)[2] == firsttime) {
+						currentstair.add(new Person(first.get(i)[0], first.get(i)[1]));
+					}
+				}
+				if (first.size() == 0) {
+					break;
+				} else {
+					firsttime++;
 				}
 			}
+
+			List<Person> currentstair2 = new ArrayList<>();
+
+			while (second.size() != 0) {
+				// 계단길이 == move 확인 -> 계단 이동 -> 계단에 도착한 사람 리스트에 넣어주기
+				//
+				for (int i = 0; i < currentstair2.size(); i++) {
+					if (currentstair2.get(i).move == stairlist.get(1).len) {
+						currentstair2.remove(i);
+						second.remove(i);
+						i--;
+					}
+				}
+
+				// 계단에 내려오게하기
+				for (int i = 0; i < currentstair2.size(); i++) {
+					if (i >= 3)
+						break;
+					else {
+						currentstair2.get(i).move++;
+					}
+				}
+				for (int i = 0; i < second.size(); i++) {
+					if (second.get(i)[2] == secondtime) {
+						currentstair2.add(new Person(second.get(i)[0], second.get(i)[1]));
+					}
+				}
+				if (second.size() == 0) {
+					break;
+				} else {
+					secondtime++;
+				}
+			}
+
+			int time = Math.max(firsttime, secondtime);
+
+			if (time < min) {
+				min = time;
+			}
+
+		} else {
+			for (int i = 0; i < 2; i++) {
+				temp[depth] = i;
+				per(temp, depth + 1);
+			}
 		}
-		return dp[stair.size()-1];
 	}
+
 }
