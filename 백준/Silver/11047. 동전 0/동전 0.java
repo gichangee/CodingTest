33bdcecf3e04afa08400e225817d709eb117999a
gichangee <93646339+gichangee@users.noreply.github.com>
2024-06-21 23:07:15 +0900

    [Silver IV] Title: 동전 0, Time: 164 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.java
+++ 백준/Silver/11047. 동전 0/동전 0.java
@@ -1,26 +1,36 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		int sum = sc.nextInt();
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st= new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		int K = Integer.parseInt(st.nextToken());
 		
-		int[] arr = new int[n];
-		for(int i=n-1;i>=0;i--) {
-			arr[i]=sc.nextInt();
+		int[] arr = new int[N];
+		
+		for(int i=0;i<N;i++) {
+			 st= new StringTokenizer(br.readLine());
+			 arr[i]=Integer.parseInt(st.nextToken());
 		}
 		
-		int count=0;
+		int count =0;
 		
-		for(int i=0;i<n;i++) {
-			count+=(sum/arr[i]);
-			sum=sum%arr[i];
+		for(int i=arr.length-1;i>=0;i--) {
+			if(arr[i]<=K) {
+				K=K-arr[i];
+				i++;
+				count++;
+			}else {
+				continue;
+			}
 		}
+		
 		
 		System.out.println(count);
 	}
-	
-	
 }
