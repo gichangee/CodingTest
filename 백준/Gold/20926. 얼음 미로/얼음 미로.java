3a9a9634a45dc0e31c4d54e8173b44d3764c0aea
gichangee <93646339+gichangee@users.noreply.github.com>
2024-06-24 22:55:21 +0900

    [Gold II] Title: 얼음 미로, Time: 520 ms, Memory: 29416 KB -BaekjoonHub

--- 백준/Gold/20926. 얼음 미로/얼음 미로.java
+++ 백준/Gold/20926. 얼음 미로/얼음 미로.java
@@ -1,113 +1,134 @@
+import java.io.*;
 import java.util.*;
- 
+
 public class Main {
- 
-    static char[][] board;
-    static int[][] dist;
-    static int w, h;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {-1, 0, 1, 0};
- 
-    public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
- 
-        //입력
-        w = scan.nextInt();
-        h = scan.nextInt();
-        scan.nextLine();
- 
-        board = new char[h][w];
-        int start_x = -1, start_y = -1;
-        int end_x = -1, end_y = -1;
-        for(int i = 0; i < h; i++) {
-            String str = scan.nextLine();
-            for(int j = 0; j < w; j++) {
-                board[i][j] = str.charAt(j);
-                if(board[i][j] == 'T') {
-                    start_x = i;
-                    start_y = j;
-                } else if(board[i][j] == 'E') {
-                    end_x = i;
-                    end_y = j;
-                }
-            }
-        }
-        //입력 끝
- 
-        int INF = Integer.MAX_VALUE;
-        dist = new int[h][w];
-        for(int i = 0; i < h; i++) {
-            Arrays.fill(dist[i], INF);
-        }
- 
-        bfs(start_x, start_y);
-        if(dist[end_x][end_y] == INF) System.out.println("-1");
-        else System.out.println(dist[end_x][end_y]);
-    }   
- 
-    public static void bfs(int x, int y) {
-        PriorityQueue<Node> q = new PriorityQueue<>();
-        boolean[][] visited = new boolean[h][w];
-        q.offer(new Node(x, y, 0));
-        dist[x][y] = 0;
- 
-        while(!q.isEmpty()) {
-            Node current = q.poll();
-        
-            if(visited[current.x][current.y]) continue;
-            visited[current.x][current.y] = true;
- 
-            for(int i = 0; i < 4; i++) {
-                int go_ratio = can_go(i, current.x, current.y);
-                if(go_ratio < 1) continue;
- 
-                int sum_dist = 0;
-                for(int j = 1; j <= go_ratio; j++) {
-                    if(board[current.x + dx[i] * j][current.y + dy[i] * j] == 'E') continue;
-                    else sum_dist += (board[current.x + dx[i] * j][current.y + dy[i] * j] - '0');
-                }
-                int nx = current.x + dx[i] * go_ratio;
-                int ny = current.y + dy[i] * go_ratio;
- 
-                if(dist[nx][ny] > sum_dist + current.time) {
-                    dist[nx][ny] = sum_dist + current.time;
-                    q.offer(new Node(nx, ny, dist[nx][ny]));
-                }
-            }
-        }
-    }
- 
-    public static int can_go(int dir, int x, int y) {
-        int ratio = 1;
-        while(true) {
-            int nx = x + dx[dir] * ratio;
-            int ny = y + dy[dir] * ratio;
- 
-            if(nx < 0 || ny < 0 || nx >= h || ny >= w) break;
-            if(board[nx][ny] == 'H') break;
-            
-            if(board[nx][ny] == 'R') return ratio - 1;
-            if(board[nx][ny] == 'E' ) return ratio;
- 
-            ratio++;
-        }
-        return -1;
-    }
- 
-    public static class Node implements Comparable<Node>{
-        int x;
-        int y;
-        int time;
- 
-        public Node(int x, int y, int time) {
-            this.x = x;
-            this.y = y;
-            this.time = time;
-        }
- 
-        @Override
-        public int compareTo(Node n) {
-            return this.time - n.time;
-        }
-    }
+
+	static int[] dy = { -1, 1, 0, 0 };
+	static int[] dx = { 0, 0, -1, 1 };
+
+	static int W, H;
+
+	static int starty, startx, endy, endx;
+
+	static char[][] map;
+
+	static class Node implements Comparable<Node> {
+		int y, x, cost, dir;
+
+		public Node(int y, int x, int cost) {
+			this.y = y;
+			this.x = x;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return this.cost - o.cost;
+		}
+
+		@Override
+		public String toString() {
+			return y + ", " + x + ", " + cost;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		W = Integer.parseInt(st.nextToken());
+		H = Integer.parseInt(st.nextToken());
+
+		map = new char[H][W];
+
+		starty = 0;
+		startx = 0;
+		endy = 0;
+		endx = 0;
+
+		for (int i = 0; i < H; i++) {
+			String s = br.readLine();
+			map[i] = s.toCharArray();
+			for (int j = 0; j < W; j++) {
+				if (map[i][j] == 'T') {
+					starty = i;
+					startx = j;
+				} else if (map[i][j] == 'E') {
+					endy = i;
+					endx = j;
+				}
+			}
+		}
+
+		int result = dijkstra(starty, startx);
+
+		if (result == Integer.MAX_VALUE) {
+			System.out.println(-1);
+			return;
+		}
+		System.out.println(result);
+	}
+
+	private static int dijkstra(int starty, int startx) {
+		PriorityQueue<Node> q = new PriorityQueue<>();
+		q.add(new Node(starty, startx, 0));
+		int[][] memo = new int[H][W];
+		for (int i = 0; i < H; i++) {
+			Arrays.fill(memo[i], Integer.MAX_VALUE);
+		}
+
+		memo[starty][startx] = 0;
+
+		while (!q.isEmpty()) {
+			Node temp = q.poll();
+
+			for (int i = 0; i < 4; i++) {
+
+				int a = 1;
+				// 해당 방향으로 미끄러져서 범위에 벗어나지 않고 도달할 수 있는지 확인
+				// 미끄러졌을 때 구멍에 빠지거나 범위에 벗어나는 경우는 해당 방향 continue
+				while (true) {
+					int goY = (dy[i] * a) + temp.y;
+					int goX = (dx[i] * a) + temp.x;
+
+					if (goY >= 0 && goX >= 0 && goY < H && goX < W) {
+						if (map[goY][goX] == 'H') {
+							a = -1;
+							break;
+						} else if (map[goY][goX] == 'R') {
+							a -= 1;
+							break;
+						} else if (map[goY][goX] == 'E') {
+							break;
+						}
+					} else {
+						a = -1;
+						break;
+					}
+					a++;
+				}
+
+				if (a == -1) {
+					continue;
+				}
+
+				int size = 0;
+				for (int j = 1; j <= a; j++) {
+					if (map[temp.y + dy[i] * j][temp.x + dx[i] * j] == 'E')
+						continue;
+					else
+						size += (map[temp.y + dy[i] * j][temp.x + dx[i] * j] - '0');
+				}
+
+				int ny = temp.y + dy[i] * a;
+				int nx = temp.x + dx[i] * a;
+
+				if (memo[ny][nx] > size + temp.cost) {
+					memo[ny][nx] = size + temp.cost;
+					q.offer(new Node(ny, nx, memo[ny][nx]));
+				}
+			}
+			
+		}
+		return memo[endy][endx];
+	}
 }
