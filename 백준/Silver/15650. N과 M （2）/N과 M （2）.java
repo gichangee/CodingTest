10dda9805d7e1dc68c4145a7054fa18852e68924
박기창 <93646339+gichangee@users.noreply.github.com>
2024-12-25 23:36:52 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -1,53 +1,39 @@
-
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.StringTokenizer;
+
 
 public class Main {
 
-	static int N;
-	static int K;
-	static int[] arr;
-	static boolean[] v;
-	static BufferedWriter bw;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
-		
-		
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		
-		arr = new int[K];
-		v= new boolean[N];
-		dfs(1,0);
-		bw.flush();
-		bw.close();
-	}
-	private static void dfs(int a,int index) throws IOException {
-		
-		if(index==K) {
-			for(int i=0;i<index;i++) {
-				bw.write(arr[i]+" ");
-			}
-			bw.write("\n");
-			return;
-		}
-		
-		
-		for(int i=a;i<=N;i++) {
-		
-			arr[index]=i;
-			dfs(i+1,index+1);
+    static int first, second;
+    static int[] num;
+    static StringBuilder stringBuilder;
 
-		}
-			
-		
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        stringBuilder = new StringBuilder();
+        StringTokenizer StringTokenizer = new StringTokenizer(br.readLine());
+        first = Integer.parseInt(StringTokenizer.nextToken());
+        second = Integer.parseInt(StringTokenizer.nextToken());
+        num = new int[second];
 
+        method(0, 1);
+
+        System.out.println(stringBuilder.toString());
+    }
+
+    private static void method(int depth, int start) {
+        if(depth == second) {
+            for (int i = 0; i < num.length; i++) {
+                stringBuilder.append(num[i]+" ");
+            }
+            stringBuilder.append("\n");
+        }else{
+            for (int i = start; i <= first; i++) {
+                    num[depth] = i;
+                    method(depth + 1, i + 1);
+            }
+        }
+    }
 }
