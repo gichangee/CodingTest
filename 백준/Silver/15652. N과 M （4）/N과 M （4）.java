29981b32af8c642e3f13619dac92358a9439a169
박기창 <93646339+gichangee@users.noreply.github.com>
2024-12-27 19:37:53 +0900

    [Silver III] Title: N과 M (4), Time: 168 ms, Memory: 19896 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -1,54 +1,46 @@
-
-
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
-import java.util.*;
+import java.util.StringTokenizer;
+
 
 public class Main {
 
-	static int N;
-	static int K;
-	static int[] arr;
-	static boolean[] v;
-	static BufferedWriter bw;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
-		
-		
-		N = Integer.parseInt(st.nextToken());
-		K = Integer.parseInt(st.nextToken());
-		
-		arr = new int[K];
-		v= new boolean[N];
-		dfs(1,0);
-		bw.flush();
-		bw.close();
-	}
-	private static void dfs(int a,int index) throws IOException {
-		
-		if(index==K) {
-			for(int i=0;i<index;i++) {
-				bw.write(arr[i]+" ");
-			}
-			bw.write("\n");
-			return;
-		}
-		
-		
-		for(int i=a;i<=N;i++) {
-		
-			arr[index]=i;
-			dfs(i,index+1);
 
-		}
-			
-		
-	}
+    static int[] num;
+    static int N,R;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        num = new int[R];
+
+        sb = new StringBuilder();
+
+        method(0,1);
+
+        System.out.println(sb.toString());
+
+    }
+
+    private static void method(int depth, int start) {
+        if(depth == R){
+            for (int i = 0; i < num.length; i++) {
+                sb.append(num[i]+" ");
+            }
+            sb.append("\n");
+        }else{
+            for (int i = start; i <= N; i++) {
+                num[depth] = i;
+                method(depth + 1, i);
+            }
+        }
+    }
+
 
 }
