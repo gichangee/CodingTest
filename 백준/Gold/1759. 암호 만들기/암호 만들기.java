196af8d0cf920c903b24c15030051931f77bb49d
박기창 <93646339+gichangee@users.noreply.github.com>
2025-01-01 20:10:14 +0900

    [Gold V] Title: 암호 만들기, Time: 112 ms, Memory: 14836 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -1,63 +1,95 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
 
-	
-	static int len,R;
-	static char[] arr,temp;
-	public static void main(String[] args) throws IOException {
+    static int L, C;
+    static String[] alphabet;
+    static String[] store;
+    static List<String[]> list;
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) throws IOException {
 
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		len = Integer.parseInt(st.nextToken());
-		R = Integer.parseInt(st.nextToken());
-		arr = new char[R];
-		String s = br.readLine();
-		String[] split = s.split(" ");
-		Arrays.sort(split);
-		for (int i = 0; i < R; i++) {
-			arr[i] = split[i].charAt(0);
-		}
-		
-		temp = new char[len];
-		combine(0,0);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	}
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        L = Integer.parseInt(st.nextToken());
+        C = Integer.parseInt(st.nextToken());
 
-	private static void combine(int cnt, int start) {
-		if(cnt == len) {
-			
-			boolean a= false;
-			int count=0;
-			for(int i=0;i<cnt;i++) {
-				if(temp[i]=='a' || temp[i]=='e' ||temp[i]=='i' ||temp[i]=='o' ||temp[i]=='u' ){
-					a=true;
-				}
-				else {
-					count++;
-				}
-			}
-			
-			if(a && count>=2) {
-				for(int i=0;i<cnt;i++) {
-					System.out.print(temp[i]);
-				}
-				System.out.println();
-			}
-			
-			
-		}else {
-			for(int i=start;i<arr.length;i++) {
-				temp[cnt]=arr[i];
-				combine(cnt+1,i+1);
-			}
-		}
-		
-	}
+        alphabet = new String[C];
+
+
+        store = new String[L];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < C; i++) {
+            alphabet[i] = st.nextToken();
+        }
+
+        list = new ArrayList<>();
+
+        method(0, 0);
+
+        Collections.sort(list, new Comparator<String[]>() {
+            @Override
+            public int compare(String[] o1, String[] o2) {
+                for (int i = 0; i < o1.length; i++) {
+                    if (o1[i].compareTo(o2[i]) > 0){
+                        return 1;
+                    }else if(o1[i].compareTo(o2[i]) < 0){
+                        return -1;
+                    }else{
+                        continue;
+                    }
+                }
+                return 0;
+            }
+        });
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.get(i).length; j++) {
+                sb.append(list.get(i)[j]);
+            }
+            sb.append("\n");
+        }
+        System.out.println(sb.toString());
+
+
+    }
+
+    private static void method(int depth, int start) {
+        if (depth == L) {
+
+            int aeiou = 0;
+            int other = 0;
+
+            String[] answer = new String[L];
+            for (int i = 0; i < store.length; i++) {
+                if(store[i].equals("a") || store[i].equals("e") || store[i].equals("i") || store[i].equals("o") || store[i].equals("u")){
+                    aeiou++;
+                }else{
+                    other++;
+                }
+                answer[i] = store[i];
+            }
+
+            if( aeiou >= 1 && other >=2){
+                Arrays.sort(answer);
+                list.add(answer);
+            }
+
+        } else {
+            for (int i = start; i < C; i++) {
+                store[depth] = alphabet[i];
+                method(depth + 1, i + 1);
+            }
+        }
+    }
+
 
 }
