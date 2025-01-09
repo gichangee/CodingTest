95f43b92dee8213c6f89a39c170fa5089ed7fef3
박기창 <93646339+gichangee@users.noreply.github.com>
2025-01-09 22:14:00 +0900

    [Silver II] Title: 로또, Time: 132 ms, Memory: 15036 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -1,73 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
-import java.util.Set;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	static int N, R;
-	static int[] arr;
-	static int[] temp;
-	static StringBuilder sb;
-	static boolean[] v;
-	static Set<String> s;
-
-	public static void main(String[] args) throws IOException {
-		Scanner sc = new Scanner(System.in);
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		
-		while(true) {
-			sb = new StringBuilder();
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			N = Integer.parseInt(st.nextToken());
-			R = 6;
-	
-			if(N==0) {
-				break;
-			}
-			arr = new int[N];
-
-			for (int i = 0; i < N; i++) {
-				arr[i] = Integer.parseInt(st.nextToken());
-			}
-
-			temp = new int[R];
-			v = new boolean[N];
-			Arrays.sort(arr);
-
-			loop(0,0);
-			System.out.println(sb.toString());
-		}
-
-		
-		
-
-	}
-
-	private static void loop(int cnt,int a) {
-		if (cnt == R) {
-
-			for (int i = 0; i < R; i++) {
-				sb.append(temp[i] + " ");
-
-			}
-			sb.append("\n");
-			return;
-
-		} else {
-
-			for (int i = a; i < N; i++) {
-
-					temp[cnt] = arr[i];
-					loop(cnt + 1,i+1);
-				}
-
-			}
-
-		}
-
+    static int[] arr;
+    static int[] store;
+    static int k;
+    static StringBuilder sb;
+    public static void main(String[] args) throws Exception{
+        // 코드를 작성해주세요
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        
+        while(true){
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            k = Integer.parseInt(st.nextToken());
+            if(k == 0){
+                break;
+            }else{
+                arr = new int[k];
+                store = new int[6];
+                sb = new StringBuilder();
+                for(int i=0;i<k;i++){
+                    arr[i] = Integer.parseInt(st.nextToken());
+                }
+                
+                method(0,0);
+                
+                System.out.println(sb.toString());
+                
+            }
+        }
+    }
+    
+    private static void method(int depth, int start){
+        if(depth == 6){
+            for(int i=0;i<6;i++){
+                sb.append(store[i]+" ");
+            }
+            sb.append("\n");
+        }else{
+            for(int i=start; i<k;i++){
+                store[depth] = arr[i];
+                method(depth+1,i+1);
+                
+            }
+        }
+    }
 }
