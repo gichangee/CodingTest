5b82697cf44347b4b1dc968b680a1d25e24b2dc9
박기창 <93646339+gichangee@users.noreply.github.com>
2025-01-10 19:13:23 +0900

    [Silver II] Title: 차이를 최대로, Time: 124 ms, Memory: 15220 KB -BaekjoonHub

--- 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
+++ 백준/Silver/10819. 차이를 최대로/차이를 최대로.java
@@ -1,61 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	static int[] arr;
-	static int[] temp;
-	static boolean[] v;
-	static int n;
-	static int max;
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		n = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		arr= new int[n];
-		
-		v= new boolean[n];
-		for(int i=0;i<n;i++) {
-			arr[i]= Integer.parseInt(st.nextToken());
-		}
-		
-		temp = new int[n];
-		loop(0);
-		System.out.println(max);
-		
-	}
-	private static void loop(int cnt) {
-		if(cnt == n) {
-			int s = sum(temp);
-			if(s>=max) {
-				max=s;
-			}
-			return;
-		}else {
-			for(int i=0;i<n;i++) {
-				if(!v[i]) {
-					v[i]=true;
-					temp[cnt]=arr[i];
-					loop(cnt+1);
-					v[i]=false;
-				}
-			}
-		}
-		
-	}
-	private static int sum(int[] arr2) {
-		int sum=0;
-		
-		for(int i=0;i<arr2.length-1;i++) {
-			//|A[0] - A[1]|
-			sum+=Math.abs(arr2[i]-arr2[i+1]);
-		}
-		
-		return sum;
-	}
-
+    
+    static int N;
+    static int[] store;
+    static int[] arr;
+    static boolean[] visited;
+    static int max = Integer.MIN_VALUE;
+    
+    public static void main(String[] args) throws Exception{
+        // 코드를 작성해주세요
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N];
+        store = new int[N];
+        visited = new boolean[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            arr[i]= Integer.parseInt(st.nextToken());
+        }
+        method(0);
+        System.out.println(max);
+    }
+    
+    private static void method(int depth){
+        if(depth == N){
+            int sum = 0;
+            for(int i=0;i<N-1;i++){
+                   sum+=Math.abs(store[i] - store[i+1]);
+            }
+            
+            if(max < sum){
+                max = sum;
+            }
+        }else{
+            for(int i=0;i<N;i++){
+                if(!visited[i]){
+                    visited[i] = true;
+                    store[depth] = arr[i];
+                    method(depth+1);
+                    visited[i] = false;
+                }
+            }
+        }
+    }
 }
