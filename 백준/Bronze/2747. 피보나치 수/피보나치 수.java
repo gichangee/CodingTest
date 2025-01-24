15a36ce4461a470a1ae8e487094c43d6f41cf5ca
박기창 <93646339+gichangee@users.noreply.github.com>
2025-01-24 21:38:23 +0900

    [Bronze II] Title: 피보나치 수, Time: 188 ms, Memory: 17712 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,22 +1,18 @@
-import java.util.Scanner;
-
+import java.util.*;
 public class Main {
-
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
-		int[] D = new int[n+1];
-		
-		D[0]=0;
-		D[1]=1;
-		
-		for(int i=2;i<=n;i++) {
-			D[i]=D[i-2]+D[i-1];
-		}
-		
-		System.out.println(D[n]);
-	}
-
+    public static void main(String[] args) {
+        // 코드를 작성해주세요
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        int[] arr = new int[46];
+        arr[0]= 0;
+        arr[1] = 1;
+        for(int i=2;i<=45;i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        
+        System.out.println(arr[n]);
+    }
+   
 }
