1cce0291382f979e6491f4afe10e31a1ccf69286
박기창 <93646339+gichangee@users.noreply.github.com>
2025-02-09 22:36:26 +0900

    [Bronze I] Title: 이항 계수 1, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
+++ 백준/Bronze/11050. 이항 계수 1/이항 계수 1.java
@@ -1,42 +1,30 @@
-
-
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	static int count;
-	static int a,b;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); 
-		
-		int N = Integer.parseInt(st.nextToken());
-		int K = Integer.parseInt(st.nextToken());
-		
-		int[][] D = new int[N+1][N+1];
-		
-		for(int i=0;i<D.length;i++) {
-			D[i][0]=1;
-			D[i][i]=1;
-			D[i][1]=i;
-		}
-		
-		for(int i=3;i<=N;i++) {
-			for(int j=2;j<i;j++) {
-				D[i][j]= D[i-1][j-1] +D[i-1][j];
-			}
-		}
-		
-		System.out.println(D[N][K]);
-		
-		
-	}
-
+    public static void main(String[] args) throws Exception{
+        // 코드를 작성해주세요
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int r = K;
+        if(K == 0 || K==N){
+            System.out.println(1);
+            return;
+        }
+        
+    
+        
+      long result = 1;
+        // 조합 공식: C(N, K) = N! / (K! * (N-K)!)
+        // 분자: N * (N-1) * ... * (N-K+1)
+        for (int i = 0; i < K; i++) {
+            result *= (N - i);
+            result /= (i + 1);  // K!에 해당하는 부분 나누기
+        }
+        
+        System.out.println(result);
+    }
 }
