0a4d2e31e42f7ac0bd410b63f16e3ae59c2980a0
박기창 <93646339+gichangee@users.noreply.github.com>
2025-02-16 23:19:16 +0900

    [Silver I] Title: 미로 탐색, Time: 108 ms, Memory: 14652 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,66 +1,67 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	static int[] dx,dy;
-	static int N,M;
-	static boolean[][] v;
-	static int[][] arr;
-	
-	public static void main(String[] args) throws IOException {
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		
-		dx = new int[]{-1,1,0,0};
-		dy = new int[]{0,0,-1,1};
-		
-		v= new boolean[N][M];
-		arr = new int[N][M];
-		for(int i=0;i<N;i++) {
-			st = new StringTokenizer(br.readLine());
-			String s = st.nextToken();
-			for(int j=0;j<M;j++) {
-				arr[i][j]=Integer.parseInt(s.substring(j,j+1));
-			}
-		}
-		
-		bfs(0,0);
-		
-		System.out.println(arr[N-1][M-1]);
-	}
-
-	private static void bfs(int i, int j) {
-		
-		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[] {i,j});
-		v[i][j]=true;
-		while(!q.isEmpty()) {
-			
-			int[] temp = q.poll();
-
-			
-			for(int k=0;k<4;k++) {
-				int x = temp[0] + dx[k];
-				int y = temp[1] + dy[k];
-				
-				if(x >= 0 && y >=0 && x<N && y<M) {
-					if(arr[x][y] != 0 && !v[x][y]) {
-						arr[x][y]=arr[temp[0]][temp[1]]+1;
-						v[x][y]=true;
-						q.add(new int[] {x,y});
-					}
-				}
-			}
-		}
-		
-	}
+    
+    private static int N,M;
+    private static int[][] arr;
+    private static int[][] memo;
+    private static boolean[][] visited; 
+    
+    private static int[] dy = {-1,1,0,0};
+    private static int[] dx = {0,0,-1,1};
+    public static void main(String[] args) throws Exception{
+        // 코드를 작성해주세요
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N+1][M+1];
+        memo = new int[N+1][M+1];
+        visited = new boolean[N+1][M+1];
+        for(int i=0;i<N;i++){
+            String s = br.readLine();
+            for(int j=0;j<M;j++){
+                arr[i+1][j+1] = s.charAt(j) - '0';
+                memo[i+1][j+1] = Integer.MAX_VALUE;
+            }
+        }
+        
+        
+        
+        bfs(1,1);
+        
+        System.out.println(memo[N][M]);
+        
+        
+    }
+    
+    private static void bfs(int starty, int startx){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{starty,startx});
+        visited[starty][startx] = true;
+        memo[1][1] = 1;
+        
+        while(!q.isEmpty()){
+            int[] temp = q.poll();
+             
+            for(int i=0;i<4;i++){
+                int goY = dy[i] + temp[0];
+                int goX = dx[i] + temp[1];
+                
+                           
+                if(goY >=1 && goX >=1 && goY <=N && goX <=M){
+                    if(arr[goY][goX] == 1){
+                        if(!visited[goY][goX]){
+                            visited[goY][goX] = true;
+                             memo[goY][goX] = memo[temp[0]][temp[1]] + 1;
+                             q.add(new int[]{goY,goX});
+                        }
+                       
+                    }
+                }
+            }
+            
+        }
+    }
 }
