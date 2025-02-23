5a64325ea43a1eef91028d8cafd984771f5d5755
박기창 <93646339+gichangee@users.noreply.github.com>
2025-02-23 21:03:57 +0900

    [Silver II] Title: 유기농 배추, Time: 140 ms, Memory: 15924 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,66 +1,68 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int M = Integer.parseInt(st.nextToken());
-			int N = Integer.parseInt(st.nextToken());
-			int K = Integer.parseInt(st.nextToken());
-			int[][] arr = new int[N][M];
-			for (int i = 0; i < K; i++) {
-				st = new StringTokenizer(br.readLine());
-				int x = Integer.parseInt(st.nextToken());
-				int y = Integer.parseInt(st.nextToken());
-				arr[y][x] = 1;
-			
-			}
-
-			boolean[][] v = new boolean[N][M];
-			int count=0;
-			
-			for(int i=0;i<N;i++) {
-				for(int j=0;j<M;j++) {
-					if(!v[i][j] && arr[i][j] ==1) {
-						v[i][j]=true;
-						bfs(arr,v,i,j);
-						count++;
-					}
-				}
-			}
-
-			System.out.println(count);
-		}
-	}
-
-	private static void bfs(int[][] arr, boolean[][] v, int y, int x) {
-
-		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[] {y,x});
-		
-		int[] dy = {-1,1,0,0};
-		int[] dx = {0,0,-1,1};
-		while(!q.isEmpty()) {
-			int[] temp = q.poll();
-			
-			for(int i=0;i<4;i++) {
-				int goY = dy[i]+temp[0];
-				int goX = dx[i]+temp[1];
-				if(goY >=0 && goX >=0 && goY < arr.length && goX <arr[0].length) {
-					if(!v[goY][goX] && arr[goY][goX]==1) {
-						v[goY][goX]=true;
-						q.add(new int[] {goY,goX});
-					}
-				}
-			}
-			
-			
-		}
-	}
-
+    
+    
+    private static int M,N,K;
+    private static int[][] arr;
+    private static boolean[][] visited;
+    
+    private static int[] dy ={-1,1,0,0};
+    private static int[] dx = {0,0,-1,1};
+    
+    public static void main(String[] args) throws Exception{
+        // 코드를 작성해주세요
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+       
+        
+        for(int i=1;i<=T;i++){
+            st = new StringTokenizer(br.readLine());
+            M = Integer.parseInt(st.nextToken());
+            N = Integer.parseInt(st.nextToken());
+            K = Integer.parseInt(st.nextToken());
+            arr = new int[N][M];
+            visited = new boolean[N][M];
+            for(int j=0;j<K;j++){
+               st = new StringTokenizer(br.readLine());
+               int X = Integer.parseInt(st.nextToken());
+               int Y = Integer.parseInt(st.nextToken());
+               arr[Y][X] = 1;
+            }
+            int sum = 0;
+            
+            for(int j=0;j<N;j++){
+                for(int k=0;k<M;k++){
+                    if(!visited[j][k] && arr[j][k] == 1){
+                        visited[j][k] = true;
+                        sum++;
+                        method(j,k);
+                    }
+                }
+            }
+            System.out.println(sum);
+        }
+    }
+    
+    private static void method(int starty,int startx){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{starty, startx});
+        while(!q.isEmpty()){
+            int[] temp = q.poll();
+            for(int i=0;i<4;i++){
+                int goY = dy[i] + temp[0];
+                int goX = dx[i] + temp[1];
+                if(goY >=0 && goX >=0 && goY< N && goX < M){
+                    if(!visited[goY][goX]){
+                        if(arr[goY][goX] == 1){
+                            visited[goY][goX] = true;
+                            q.add(new int[]{goY,goX});
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
