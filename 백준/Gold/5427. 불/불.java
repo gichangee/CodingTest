7d8d162fa8add3023f07c18e856e8b5defac117b
박기창 <93646339+gichangee@users.noreply.github.com>
2025-02-27 21:27:28 +0900

    [Gold IV] Title: 불, Time: 596 ms, Memory: 91948 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -2,112 +2,109 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	static int min;
-	static boolean a;
-
-	public static void main(String[] args) throws NumberFormatException, IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int T = Integer.parseInt(br.readLine());
-		for (int tc = 1; tc <= T; tc++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			int w = Integer.parseInt(st.nextToken());
-			int h = Integer.parseInt(st.nextToken());
-			char[][] arr = new char[h][w];
-			for (int i = 0; i < h; i++) {
-				arr[i] = br.readLine().toCharArray();
-			}
-			int starty = 0;
-			int startx = 0;
-
-			List<int[]> list = new ArrayList<>();
-
-			for (int i = 0; i < h; i++) {
-				for (int j = 0; j < w; j++) {
-					if (arr[i][j] == '@') {
-						starty = i;
-						startx = j;
-					}
-					if (arr[i][j] == '*') {
-						list.add(new int[] { i, j });
-					}
-				}
-			}
-
-			min = Integer.MAX_VALUE;
-			a = false;
-			bfs(arr, list, starty, startx);
-
-			if (!a) {
-				System.out.println("IMPOSSIBLE");
-			} else {
-				System.out.println(min);
-			}
-		}
-
-	}
-
-	private static void bfs(char[][] arr, List<int[]> list, int starty, int startx) {
-
-		Queue<int[]> fire = new ArrayDeque<int[]>();
-		Queue<int[]> q = new ArrayDeque<int[]>();
-		q.add(new int[] { starty, startx, 1});
-		boolean[][] v = new boolean[arr.length][arr[0].length];
-		v[starty][startx] = true;
-
-		int[] dy = new int[] { -1, 1, 0, 0 };
-		int[] dx = new int[] { 0, 0, -1, 1 };
-
-		for (int i = 0; i < list.size(); i++) {
-			fire.add(new int[] { list.get(i)[0], list.get(i)[1], 1 });
-		}
-
-		while (!q.isEmpty()) {
-			int[] temp = q.poll();
-			int count = fire.size();
-
-			if (temp[0] == 0 || temp[0] == arr.length - 1 || temp[1] == 0 || temp[1] == arr[0].length - 1) {
-				a = true;
-				min = temp[2];
-				break;
-			}
-
-			int[] check = fire.peek();
-			if(!fire.isEmpty()) {
-				if(check[2]<=temp[2]) {
-					for (int fc = 0; fc < count; fc++) {
-						int[] tempfire = fire.poll();
-						for (int i = 0; i < 4; i++) {
-							int goY = dy[i] + tempfire[0];
-							int goX = dx[i] + tempfire[1];
-							if (goY >= 0 && goX >= 0 && goY < arr.length && goX < arr[0].length) {
-								if (arr[goY][goX] == '.' ||arr[goY][goX] == '@' ) {
-									arr[goY][goX] = '*';
-									fire.add(new int[] { goY, goX ,tempfire[2]+1});
-								}
-							}
-						}
-					}
-				}
-				
-			}
-			
-
-			for (int i = 0; i < 4; i++) {
-				int goY = dy[i] + temp[0];
-				int goX = dx[i] + temp[1];
-				if (goY >= 0 && goX >= 0 && goY < arr.length && goX < arr[0].length) {
-					if (arr[goY][goX] == '.') {
-						if (!v[goY][goX]) {
-							v[goY][goX] = true;
-							q.add(new int[] { goY, goX,temp[2]+1});
-						}
-					}
-
-				}
-			}
-		}
-	}
-
+    private static char[][] arr;
+    private static boolean[][] visited;
+    private static int[] dy = {-1,1,0,0};
+    private static int[] dx = {0,0,-1,1};
+    private static int time;
+    private static int w,h;
+    private static Queue<int[]> fire = new LinkedList<>();
+    
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        for(int t = 1; t<=T;t++){
+            st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+            arr = new char[h][w];
+            fire = new LinkedList<>();
+            visited = new boolean[h][w];
+            for(int i=0;i<h;i++){
+                String s = br.readLine();
+                arr[i] = s.toCharArray();
+            }
+            
+            int starty = -1;
+            int startx = -1;
+            
+            for(int i=0;i<h;i++){
+                for(int j=0;j<w;j++){
+                    if(arr[i][j] == '@'){
+                        starty = i;
+                        startx = j;
+                    }
+                    
+                    if(arr[i][j] == '*'){
+                        fire.add(new int[]{i,j});
+                        visited[i][j] = true;
+                    }
+                }
+            }
+            
+            time = 0;
+            
+            int result = method(starty,startx);
+            
+            if(result == -1){
+                System.out.println("IMPOSSIBLE");
+            }else{
+                System.out.println(result+1);
+            }
+        }
+    }
+    
+    private static int method(int starty, int startx){
+        Queue<int []>q = new LinkedList<>();
+        q.add(new int[]{starty,startx,0});
+        while(!q.isEmpty()){
+            int[] temp = q.poll();
+            
+            if(temp[2] == time){
+                fire();
+                time++;
+            }
+            
+            if(temp[0] == 0 || temp[1] == 0 || temp[0] == h-1 || temp[1] == w-1){
+                return temp[2];
+            }
+            
+            for(int i=0;i<4;i++){
+                int goY = temp[0] + dy[i];
+                int goX = temp[1] + dx[i];
+                if(goY>=0 && goX >=0 && goY < h && goX <w){
+                    if(!visited[goY][goX]){
+                        if(arr[goY][goX] != '#' && arr[goY][goX] != '*'){
+                            visited[goY][goX] = true;
+                            q.add(new int[]{goY,goX,temp[2]+1});
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
+    }
+    
+    private static void fire(){
+        int size = fire.size();
+        for(int t = 0;t<size;t++){
+            int[] temp = fire.poll();
+            
+            for(int i=0;i<4;i++){
+                int goY = temp[0] + dy[i];
+                int goX = temp[1] + dx[i];
+                
+                if(goY>=0 && goX >=0 && goY < h && goX <w){
+                    if(!visited[goY][goX]){
+                        if(arr[goY][goX] != '#'){
+                            arr[goY][goX] = '*';
+                            visited[goY][goX] = true;
+                            fire.add(new int[]{goY,goX});
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
