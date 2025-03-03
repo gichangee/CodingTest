5f164194e558f33a08678d48aa8febd10de39f35
박기창 <93646339+gichangee@users.noreply.github.com>
2025-03-03 23:17:27 +0900

    [Silver I] Title: 스타트링크, Time: 180 ms, Memory: 62372 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.java
+++ 백준/Silver/5014. 스타트링크/스타트링크.java
@@ -1,51 +1,58 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st= new StringTokenizer(br.readLine());
-		
-		int F = Integer.parseInt(st.nextToken());
-		int S = Integer.parseInt(st.nextToken());
-		int G = Integer.parseInt(st.nextToken());
-		int U = Integer.parseInt(st.nextToken());
-		int D = Integer.parseInt(st.nextToken());
-		
-		boolean[] v= new boolean[1000001];
-		
-		Queue<int[]> q= new ArrayDeque<>();
-		
-		q.add(new int[] {S,0});
-		
-		while(!q.isEmpty()) {
-			int[] temp = q.poll();
-			
-			if(temp[0]==G) {
-				System.out.println(temp[1]);
-				System.exit(0);
-			}
-			
-			for(int i=0;i<2;i++) {
-				if(i==0 && temp[0]+U <=F &&!v[temp[0]+U]) {
-					v[temp[0]+U]=true;
-					q.add(new int[] {temp[0]+U,temp[1]+1});
-				}else if(i==1 && temp[0]-D >0 && !v[temp[0]-D]){
-					v[temp[0]-D]=true;
-					q.add(new int[] {temp[0]-D,temp[1]+1});
-				}
-			}
-			
-		}
-		
-		System.out.println("use the stairs");
-		
-	}
-
+    public static void main(String[] args) throws Exception{
+        // 코드를 작성해주세요
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int F = Integer.parseInt(st.nextToken());
+        int S = Integer.parseInt(st.nextToken());
+        int G = Integer.parseInt(st.nextToken());
+        int U = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        
+        if(G == S){
+            System.out.println(0);
+            return;
+        }
+        
+        boolean[] visited = new boolean[F+1];
+        
+        visited[S] = true;
+        visited[0] = true;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{S,0});
+        while(!q.isEmpty()){
+            int[] temp = q.poll();
+            
+            if(temp[0] == G){
+                System.out.println(temp[1]);
+                return;
+            }
+            
+            int goU = temp[0] + U;
+            int goD = temp[0] - D;
+            if(goU <=F && goU >=1){
+                if(!visited[goU]){
+                    visited[goU] = true;
+                    q.add(new int[]{goU,temp[1]+1});
+                }
+            }
+            
+             if(goD <=F && goD >=1){
+                if(!visited[goD]){
+                    visited[goD] = true;
+                    q.add(new int[]{goD,temp[1]+1});
+                }
+            }
+        }
+        
+                
+        
+        
+        System.out.println("use the stairs");
+        
+        
+    }
 }
