30aad9592851086057ef780025ef7c66bd27a367
박기창 <93646339+gichangee@users.noreply.github.com>
2025-03-06 21:55:24 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 776 ms, Memory: 184844 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -2,118 +2,58 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
-		BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st= new StringTokenizer(br.readLine());
-		
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		
-		int[][] arr = new int[N][M];
-		int[][][] v = new int[N][M][2];
-		
-		for(int i=0;i<N;i++) {
-			String s =br.readLine();
-			String[] split = s.split("");
-			for(int j=0;j<M;j++) {
-				arr[i][j]=Integer.parseInt(split[j]);
-			}
-		}
-		
-		for(int i=0;i<N;i++) {
-			for(int j=0;j<M;j++) {
-				v[i][j][0]= Integer.MAX_VALUE;
-				v[i][j][1]= Integer.MAX_VALUE;
-			}
-		}
-		
-		
-		
-		bfs(arr,v);
-
-	}
-
-	private static void bfs(int[][] arr, int[][][] v) {
-
-		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[] {0,0,1,1});
-		
-		
-		for(int i=1;i>=0;i--) {
-			v[0][0][i]=1;
-		}
-		
-		
-		int[] dy = {-1,1,0,0};
-		int[] dx = {0,0,-1,1};
-		while(!q.isEmpty()) {
-			int[] temp = q.poll();
-			
-
-			int min = Integer.MAX_VALUE;
-			if(temp[0] == arr.length-1 && temp[1] == arr[0].length-1) {
-				for(int i=0;i<2;i++) {
-					if(min > v[temp[0]][temp[1]][i]) {
-						min = v[temp[0]][temp[1]][i];
-					}
-				}
-				
-				System.out.println(min);
-				return;
-			}
-			
-			//벽을 부수기 가능
-			if(temp[3]==1) {
-				
-				for(int i=0;i<4;i++) {
-					int goY = temp[0]+dy[i];
-					int goX = temp[1]+dx[i];
-					if(goY >=0 && goX >=0 && goY <arr.length && goX <arr[0].length) {
-						if(arr[goY][goX] == 1) {
-							if(v[goY][goX][0] > temp[2]+1) {
-								v[goY][goX][0] = temp[2]+1;
-								q.add(new int[] {goY,goX,temp[2]+1,0});
-							}
-						}
-						
-					}
-				}
-				
-				for(int i=0;i<4;i++) {
-					int goY = temp[0]+dy[i];
-					int goX = temp[1]+dx[i];
-					if(goY >=0 && goX >=0 && goY <arr.length && goX <arr[0].length) {
-						if(arr[goY][goX]==0) {
-							if(v[goY][goX][1] > temp[2]+1) {
-								v[goY][goX][1]= temp[2]+1;
-								q.add(new int[] {goY,goX,temp[2]+1,1});
-							}
-						}
-					}
-				}
-				
-			}
-			//벽 부시기 불가능
-			else if(temp[3]==0) {
-				for(int i=0;i<4;i++) {
-					int goY = temp[0]+dy[i];
-					int goX = temp[1]+dx[i];
-					if(goY >=0 && goX >=0 && goY <arr.length && goX <arr[0].length) {
-						if(arr[goY][goX]==0) {
-							if(v[goY][goX][0] > temp[2]+1) {
-								v[goY][goX][0]= temp[2]+1;
-								q.add(new int[] {goY,goX,temp[2]+1,0});
-							}
-						}
-					}
-				}
-			}
-		
-		}
-		System.out.println(-1);
-		
-	}
-
+    private static int[] dy = {-1,1,0,0};
+    private static int[] dx = {0,0,-1,1};
+    private static boolean[][][] visited;
+    private static int[][] arr;
+    private static int N,M;
+    public static void main(String[] args) throws Exception{
+        // 코드를 작성해주세요
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N][M];
+        visited = new boolean[N][M][2];
+        for(int i=0;i<N;i++){
+            String s  = br.readLine();
+            for(int j=0;j<M;j++){
+                arr[i][j] = Integer.parseInt(s.charAt(j)+"");
+            }
+        }
+        
+        int count = method(0,0);
+        System.out.println(count);
+    }
+    
+    private static int method(int starty, int startx){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{starty, startx,1,1});
+        while(!q.isEmpty()){
+            int[] temp = q.poll();
+            
+            if(temp[0] == N-1 && temp[1] == M-1){
+                return temp[3];
+            }
+            
+            for(int i=0;i<4;i++){
+                int goY = dy[i] + temp[0];
+                int goX = dx[i] + temp[1];
+                if(goY >=0 && goX >=0 && goX < M && goY < N){
+                    if(arr[goY][goX] == 0){
+                        if(!visited[goY][goX][temp[2]]){
+                            visited[goY][goX][temp[2]] = true;
+                            q.add(new int[]{goY,goX,temp[2],temp[3]+1});
+                        }
+                    }else{
+                        if(!visited[goY][goX][0] && temp[2] == 1){
+                            visited[goY][goX][0] = true;
+                            q.add(new int[]{goY,goX,0,temp[3]+1});
+                        }
+                    }
+                }
+            }
+        }
+        return -1;
+    }
 }
