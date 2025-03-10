5b7d22c69f0a9f6907969da2926514a7d511e22f
박기창 <93646339+gichangee@users.noreply.github.com>
2025-03-10 21:56:51 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 488 ms, Memory: 89388 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.java
@@ -2,120 +2,79 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-	
-	
-	static int[] dy = {-1,1,0,0};
-	static int[] dx = {0,0,-1,1};
-	
-	static int[] hy = {-1,-2,-2,-1,1,2,2,1};
-	static int[] hx = {-2,-1,1,2,2,1,-1,-2};
-	
-	static int[][] arr;
-	static int[][][] v;
-	
-	static int H,W,K;
-	
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
-		K= Integer.parseInt(br.readLine());
-		StringTokenizer st =new StringTokenizer(br.readLine());
-		W = Integer.parseInt(st.nextToken());
-		H = Integer.parseInt(st.nextToken());
-		
-		arr = new int[H][W];
-		v = new int[H][W][K+1];
-		
-		for(int i=0;i<H;i++) {
-			st =new StringTokenizer(br.readLine());
-			for(int j=0;j<W;j++) {
-				arr[i][j]=Integer.parseInt(st.nextToken());
-			}
-		}
-
-		
-		for(int i=0;i<H;i++) {
-			for(int j=0;j<W;j++) {
-				for(int z = 0; z<=K;z++) {
-					v[i][j][z] = Integer.MAX_VALUE;
-				}
-			}
-		}
-		
-		dfs();
-
-
-		
-		
-	}
-
-	private static void dfs() {
-
-		Queue<int[]> q = new ArrayDeque<>();
-		q.add(new int[] {0,0,K,0});
-		for(int i=K;i>=0;i--) {
-			v[0][0][i]=0;
-		}
-		
-		while(!q.isEmpty()) {
-			int[] temp = q.poll();
-			
-			if(temp[0] == H-1 && temp[1] == W-1) {
-				int min = Integer.MAX_VALUE;
-				for(int i=K;i>=0;i--) {
-					if(min > v[H-1][W-1][i]) {
-						min =  v[H-1][W-1][i];
-					}
-				}
-				System.out.println(min);
-				return;
-			}
-			
-			int count = temp[2];
-			//아직 말처럼 움직일 수 있을 때
-			if(count >0) {
-				for(int i=0;i<8;i++) {
-					int goY = hy[i]+temp[0];
-					int goX = hx[i]+temp[1];
-					if(goY >=0 && goY <arr.length && goX >=0 && goX < arr[0].length) {
-						if(arr[goY][goX] == 0 ) {
-							if(temp[3]+1 < v[goY][goX][count-1]) {
-								v[goY][goX][count-1] = temp[3]+1;
-								q.add(new int[] {goY,goX,count-1,temp[3]+1});
-							}
-						}
-					}
-				}
-				
-				for(int i=0;i<4;i++) {
-					int goY = dy[i]+temp[0];
-					int goX = dx[i]+temp[1];
-					if(goY >=0 && goY <arr.length && goX >=0 && goX < arr[0].length) {
-						if(arr[goY][goX] == 0 ) {
-							if(temp[3]+1 < v[goY][goX][count]) {
-								v[goY][goX][count] = temp[3]+1;
-								q.add(new int[] {goY,goX,count,temp[3]+1});
-							}
-						}
-					}
-				}
-			}
-			//말처럼 움직일 수 없을 때
-			if(count ==0) {
-				for(int i=0;i<4;i++) {
-					int goY = dy[i]+temp[0];
-					int goX = dx[i]+temp[1];
-					if(goY >=0 && goY <arr.length && goX >=0 && goX < arr[0].length) {
-						if(arr[goY][goX] == 0 ) {
-							if(temp[3]+1 < v[goY][goX][0]) {
-								v[goY][goX][0] = temp[3]+1;
-								q.add(new int[] {goY,goX,0,temp[3]+1});
-							}
-						}
-					}
-				}
-			}
-		}
-		
-		System.out.println(-1);
-	}
+    private static int[] dy ={-1,-2,-2,-1,1,2,2,1};
+    private static int[] dx = {-2,-1,1,2,2,1,-1,-2};
+    
+    private static int[] my = {-1,1,0,0};
+    private static int[] mx = {0,0,-1,1};
+    
+    private static int K,W,H;
+    private static int[][] arr;
+    private static boolean[][][] visited;
+    public static void main(String[] args) throws Exception{
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        K = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        W = Integer.parseInt(st.nextToken());
+        H = Integer.parseInt(st.nextToken());
+        
+        arr = new int[H][W];
+        visited = new boolean[H][W][K+1];
+        for(int i=0;i<H;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<W;j++){
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int i=0;i<K;i++){
+            visited[0][0][i] = true;
+        }
+     
+        System.out.println(bfs(0,0,K));
+        
+    }
+    
+    private static int bfs(int starty,int startx,int count){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{starty,startx,count,0});
+        while(!q.isEmpty()){
+            int[] temp = q.poll();
+            
+            if(temp[0] == H-1 && temp[1] == W-1){
+                return temp[3];
+            }
+            
+            if(temp[2] > 0){
+                 for(int i=0;i<8;i++){
+                    int goY = dy[i] + temp[0];
+                    int goX = dx[i] + temp[1];
+                    if(goY >=0 && goX >=0 && goY < H && goX < W){
+                        if(!visited[goY][goX][temp[2]-1]){
+                            if(arr[goY][goX] == 0){
+                                visited[goY][goX][temp[2]-1] = true;
+                                q.add(new int[]{goY,goX,temp[2]-1,temp[3]+1});
+                            }
+                          
+                        }
+                    }
+                }
+            }
+            for(int i=0;i<4;i++){
+                    int goY = my[i] + temp[0];
+                    int goX = mx[i] + temp[1];
+                    if(goY >=0 && goX >=0 && goY < H && goX < W){
+                        if(!visited[goY][goX][temp[2]]){
+                            if(arr[goY][goX] == 0){
+                                visited[goY][goX][temp[2]] = true;
+                                q.add(new int[]{goY,goX,temp[2],temp[3]+1});
+                            }
+                        }
+                    }
+            }
+ 
+        }
+        return -1;
+    }
 }
