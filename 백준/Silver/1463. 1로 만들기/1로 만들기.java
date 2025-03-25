4914e8f60e36fdf7f176b405f3fe267a2e9feca7
박기창 <93646339+gichangee@users.noreply.github.com>
2025-03-25 21:47:35 +0900

    [Silver III] Title: 1로 만들기, Time: 120 ms, Memory: 18224 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,39 +1,59 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	public static void main(String[] args) {
-
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
-		int[] D = new int[n+3];
-		
-		D[1]=0;
-		D[2]=1;
-		D[3]=1;
-		
-		
-		
-		for(int i=4;i<=n;i++) {
-			if(i%3==0 && i%2==0) {
-				//총 3가지 비교
-				D[i] = Math.min(Math.min(D[i/3],D[i/2]), Math.min(D[i/2],D[i-1]))+1;
-			}else if(i%3==0) {
-				//총 2가지 비교
-				D[i] = Math.min(D[i-1], D[i/3])+1;
-			}else if(i%2==0) {
-				//총 2가지 비교
-				D[i] = Math.min(D[i-1], D[i/2])+1;
-			}else {
-				D[i] = D[i-1]+1;
-			}
-		}
-		
-		
-		System.out.println(D[n]);
-		
-	}
-
+    
+    private static int N;
+    private static int[] arr;
+    
+    public static void main(String[] args) throws Exception{
+        // 코드를 작성해주세요
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        
+        arr = new int[N+1];
+        
+        Arrays.fill(arr,Integer.MAX_VALUE);
+        
+        int count = method();
+        System.out.println(count);
+    }
+    private static int method(){
+        PriorityQueue<int[]> q = new PriorityQueue<>((a,b) -> a[1] - b[1]);
+        
+        q.add(new int[]{N,0});
+        
+        while(!q.isEmpty()){
+            int[] temp = q.poll();
+            
+            if(temp[0] == 1){
+                return temp[1];
+            }
+            
+            if(temp[0] % 3 == 0 && arr[temp[0]/3] > temp[1]+1){
+                arr[temp[0]/3] = temp[1]+1;
+                q.add(new int[]{temp[0]/3,temp[1]+1});
+            }
+            
+            if(temp[0] % 2 == 0 && arr[temp[0]/2] > temp[1]+1){
+                arr[temp[0]/2] = temp[1]+1;
+                q.add(new int[]{temp[0]/2,temp[1]+1});
+            }
+            
+            if(temp[0] -1 >0 && arr[temp[0]-1] > temp[1]+1){
+                arr[temp[0]-1] = temp[1]+1;
+                q.add(new int[]{temp[0]-1,temp[1]+1});
+            }
+            
+            
+            
+        }
+        
+        return -1;
+        
+        
+        
+        
+    }
+    
 }
