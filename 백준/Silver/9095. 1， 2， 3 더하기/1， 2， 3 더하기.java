f4b8ddbbe07882c08855f16163cc463798e7ed75
박기창 <93646339+gichangee@users.noreply.github.com>
2025-03-30 22:48:01 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 116 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,59 +1,49 @@
-
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-	static int sum;
-	static int[] arr = { 1, 2, 3 };
-	static int[] temp;
-	static int count;
-
-	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		StringBuilder sb = new StringBuilder();
-		int t = Integer.parseInt(st.nextToken());
-
-		for (int i = 0; i < t; i++) {
-			count = 0;
-			sum = Integer.parseInt(br.readLine());
-			for (int j = 1; j <= sum; j++) {
-				temp = new int[j];
-				//N = 3
-				//temp.length 가 R
-				loop(0);
-			}
-			sb.append(count+"\n");
-
-		}
-		
-		System.out.println(sb.toString());
-	}
-
-	private static void loop(int cnt) {
-		if (cnt == temp.length) {
-
-			int tempsum = 0;
-			for (int i = 0; i < temp.length; i++) {
-				tempsum += temp[i];
-			}
-
-			if (tempsum == sum) {
-				count++;
-			}
-
-		} else {
-			for (int i = 1; i <= 3; i++) {
-				temp[cnt] = i;
-				loop(cnt+1);
-			}
-		}
-
-	}
+    private static int[] store,arr;
+    private static int n,c;
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+        int T = Integer.parseInt(s);
+        
+        arr = new int[3];
+    
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        for(int t = 1; t<=T;t++){
+            s = br.readLine();
+            n = Integer.parseInt(s);
+            c= 0;
+            for(int i=1;i<=n;i++){
+                store = new int[i];
+                method(0,i);
+            }
+            
+            System.out.println(c);
+            
+        }
+    }
+    private static void method(int depth, int r){
+        if(depth == r){
+            int sum = 0;
+            for(int i=0;i<store.length;i++){
+                sum+=store[i];
+            }
+            
+            if(sum == n){
+                c++;
+            }     
+            
+        }else{
+            for(int i=0;i<3;i++){
+                store[depth] = arr[i];
+                method(depth+1,r);
+            }
+        }
+    }
+    
 }
