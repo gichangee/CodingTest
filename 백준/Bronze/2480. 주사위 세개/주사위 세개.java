635d52124478420f71f9f7d6084b2268a88767cd
박기창 <93646339+gichangee@users.noreply.github.com>
2025-04-03 19:53:35 +0900

    [Bronze IV] Title: 주사위 세개, Time: 172 ms, Memory: 17704 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,33 +1,31 @@
-import java.io.*;
 import java.util.*;
-
 public class Main {
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int a = Integer.parseInt(st.nextToken());
-		int b = Integer.parseInt(st.nextToken());
-		int c = Integer.parseInt(st.nextToken());
-		
-		if(a==b && b==c) {
-			System.out.println(10000+(a*1000));
-			return;
-		}
-		else if(a !=b && a !=c && b!=c) {
-			int x = Math.max(a, b);
-			System.out.println(Math.max(x, c)*100);
-			return;
-		}else if( a== b && b !=c){
-			System.out.println(1000+(a*100));
-			return;
-		}else if( a== c && a!=b) {
-			System.out.println(1000+(a*100));
-			return;
-		}else if(b==c && a!=b) {
-			System.out.println(1000+(b*100));
-			return;
-		}
-	}
-
+    public static void main(String[] args) {
+        // 코드를 작성해주세요
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        int C = sc.nextInt();
+        
+        int sum = 0;
+        
+        if(A == B && A == C){
+            sum = 10000 + A * 1000;
+        }else if(A != B && A != C && B != C){
+            
+            sum = Math.max(Math.max(A,B),C);
+            sum = sum * 100;
+        }else{
+            if(A == B){
+                sum = 1000 + A * 100;
+            }else if(A==C){
+                 sum = 1000 + A * 100;
+            }else{
+                 sum = 1000 + B * 100;
+            }
+        }
+        
+        System.out.println(sum);
+        
+    }
 }
