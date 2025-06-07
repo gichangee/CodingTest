6664f4b9b080533fbfb325cf372975655f37764b
박기창 <93646339+gichangee@users.noreply.github.com>
2025-06-07 19:10:23 +0900

    [Bronze V] Title: 검증수, Time: 180 ms, Memory: 17668 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -1,29 +1,19 @@
-import java.util.*;
-import java.io.*;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
 
-	public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException {
+        //input
+        Scanner scanner = new Scanner(System.in);
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		int a = Integer.parseInt(st.nextToken());
-		a= (int) Math.pow(a, 2);
-		
-		int b = Integer.parseInt(st.nextToken());
-		b= (int) Math.pow(b, 2);
-		
-		int c = Integer.parseInt(st.nextToken());
-		c= (int) Math.pow(c, 2);
-		
-		int d = Integer.parseInt(st.nextToken());
-		d= (int) Math.pow(d, 2);
-		
-		int e = Integer.parseInt(st.nextToken());
-		e= (int) Math.pow(e, 2);
-		
-		int sum = (a+b+c+d+e)%10;
-		System.out.println(sum);
-	}
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int temp = scanner.nextInt();
+            sum += temp * temp;
+        }
 
+        int result = sum % 10;
+        System.out.println(result);
+    }
 }
